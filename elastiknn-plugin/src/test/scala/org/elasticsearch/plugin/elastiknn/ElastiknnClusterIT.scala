c2274074fbc5de319c62cd6d9bd134cec90b0749
Alex Klibisz <aklibisz@gmail.com>
2019-11-02 17:20:44 -0400

    Better testing

--- elastiknn-plugin/src/test/scala/org/elasticsearch/plugin/elastiknn/ElastiknnClusterIT.scala
+++ elastiknn-plugin/src/test/scala/org/elasticsearch/plugin/elastiknn/ElastiknnClusterIT.scala
@@ -6,27 +6,17 @@ import java.util.Collections
 import com.carrotsearch.randomizedtesting.annotations.ThreadLeakScope
 import com.sksamuel.elastic4s.ElasticClient
 import com.sksamuel.elastic4s.ElasticDsl._
-import com.sksamuel.elastic4s.http.JavaClient
 import org.elasticsearch.plugins.Plugin
 import org.elasticsearch.test.ESIntegTestCase
 import org.junit.Assert._
-import org.junit.Before
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
 @ThreadLeakScope(ThreadLeakScope.Scope.NONE) // https://discuss.elastic.co/t/integration-testing-in-java-carrotsearch-thread-leaks-severe-errors/26831/4
-class ElastiknnClusterIT extends ESIntegTestCase with AsyncTesting {
+class ElastiknnClusterIT extends ESIntegTestCase with TestingMixins {
 
-  @Before
-  override def setUp(): Unit = {
-    this.ensureGreen()
-    super.setUp()
-  }
-
-  private lazy val client: ElasticClient = {
-    val javaClient = JavaClient.fromRestClient(ESIntegTestCase.getRestClient)
-    ElasticClient(javaClient)
-  }
+  private lazy val client: ElasticClient = elasticClient(
+    ESIntegTestCase.getRestClient)
 
   override def nodePlugins(): util.Collection[Class[_ <: Plugin]] =
     Collections.singletonList(classOf[ElastiKnnPlugin])
--- elastiknn-plugin/src/test/scala/org/elasticsearch/plugin/elastiknn/TestingMixins.scala
+++ elastiknn-plugin/src/test/scala/org/elasticsearch/plugin/elastiknn/TestingMixins.scala
@@ -1,11 +1,18 @@
 package org.elasticsearch.plugin.elastiknn
 
-import scala.concurrent.{Await, Future}
-import scala.concurrent.duration._
+import com.sksamuel.elastic4s.ElasticClient
+import com.sksamuel.elastic4s.http.JavaClient
+import org.elasticsearch.client.RestClient
 
-trait AsyncTesting {
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+
+trait TestingMixins {
 
   def defaultAwaitDuration: Duration = 10.seconds
+
+  def elasticClient(rc: RestClient): ElasticClient =
+    ElasticClient(JavaClient.fromRestClient(rc))
 
   final def await[T](f: => Future[T]): Unit = {
     Await.result(f, defaultAwaitDuration)
