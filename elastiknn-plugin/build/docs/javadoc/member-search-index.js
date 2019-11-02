30dbe20d76687287d7f731e723221f04be94f92f
Alex Klibisz <aklibisz@gmail.com>
2019-11-01 22:33:32 -0400

    rm build

--- elastiknn-plugin/build/docs/javadoc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"org.elasticsearch.plugin","c":"ElastiKnnPlugin","l":"ElastiKnnPlugin()","url":"%3Cinit%3E()"},{"p":"org.elasticsearch.plugin","c":"ElastiKnnPlugin","l":"getProcessors(Processor.Parameters)","url":"getProcessors(org.elasticsearch.ingest.Processor.Parameters)"}]
--- elastiknn-plugin/build/docs/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.elasticsearch.plugin","l":"ElastiKnnPlugin"}]
