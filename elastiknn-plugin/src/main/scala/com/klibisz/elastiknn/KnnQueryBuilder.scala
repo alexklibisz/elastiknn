1fac75ffed2742c313726d81f7bd7cdc572cd340
Alex Klibisz <aklibisz@gmail.com>
2019-11-08 01:09:23 -0500

    Added comments with examples of working script queries

--- elastiknn-plugin/src/main/scala/com/klibisz/elastiknn/KnnQueryBuilder.scala
+++ elastiknn-plugin/src/main/scala/com/klibisz/elastiknn/KnnQueryBuilder.scala
@@ -50,10 +50,35 @@ final class KnnQueryBuilder(query: KNearestNeighborsQuery) extends AbstractQuery
     * Hits here right after Parser#fromXContent. This is supposed to return a Lucene query, and I'm not sure what happens after that.
     */
   override def doToQuery(context: QueryShardContext): Query = {
+    // Will need to write the exact queries as script scoring queries.
     // https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-script-score-query.html
-    // For exact queries, some of the distance functions are already implemented:
-    // https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-script-score-query.html#vector-functions
-    // Look for cosineSimilarity(), l1norm(), and l2norm().
+    // The docs show some tempting vector functions (cosineSimilarity, l1Norm, etc.), but those are only available with X-Pack.
+    // This seems to work without any special mappings.
+    // {
+    //    "query": {
+    //        "function_score": {
+    //            "script_score": {
+    //                "script": {
+    //                    "lang": "painless",
+    //                    "inline": "float total = 0; for (int i = 0; i < doc['vec_raw'].length; ++i) { total += doc['vec_raw'][i]; } return total;"
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
+    //{
+    //    "query": {
+    //        "function_score": {
+    //            "script_score": {
+    //                "script": {
+    //                    "lang": "painless",
+    //                    "inline": "float total = 0; for (int i = 0; i < doc['vec_proc.exact.vector'].length; ++i) { total += doc['vec_proc.exact.vector'][i]; } return total;"
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
+
     // If you end up needing to access other documents while executing the query, for example to retrieve the pipeline
     // that was used to ingest the document, then a similar pattern is used for geo_polygon queries against an indexed
     // shape. Look at AbstractGeometryQueryBuilder.java in the doRewrite function on line 491. It also looks like you
@@ -61,12 +86,9 @@ final class KnnQueryBuilder(query: KNearestNeighborsQuery) extends AbstractQuery
 
 //    context.registerAsyncAction(new BiConsumer[Client, ActionListener[_]] {
 //      override def accept(client: Client, listener: ActionListener[_]): Unit = {
-//
 //        ???
 //      }
 //    })
-
-    println(this.query)
 
     ???
   }
