243a6202cd67eea758ca3186b50307178b38de40
Alex Klibisz <aklibisz@gmail.com>
2019-11-25 21:59:05 -0500

    fixed protobuf utils test

--- testing/src/test/scala/com/klibisz/elastiknn/utils/ProtobufUtilsTests.scala
+++ testing/src/test/scala/com/klibisz/elastiknn/utils/ProtobufUtilsTests.scala
@@ -2,6 +2,7 @@ package com.klibisz.elastiknn.utils
 
 import com.klibisz.elastiknn.Distance.DISTANCE_ANGULAR
 import com.klibisz.elastiknn.ProcessorOptions.ModelOptions.Lsh
+import com.klibisz.elastiknn.VectorType.VECTOR_TYPE_DOUBLE
 import com.klibisz.elastiknn.utils.ProtobufUtils._
 import com.klibisz.elastiknn.{LshModelOptions, ProcessorOptions}
 import org.scalatest.{FunSuite, Matchers}
@@ -13,25 +14,27 @@ class ProtobufUtilsTests extends FunSuite with Matchers {
   test("converting a pb message to a java map") {
 
     val procOptActual = ProcessorOptions(
-      fieldRaw = "field raw",
+      fieldRaw = "field_raw",
       dimension = 222,
+      vectorType = VECTOR_TYPE_DOUBLE,
       modelOptions = Lsh(
         LshModelOptions(
           seed = 99,
           distance = DISTANCE_ANGULAR,
           k = 22,
-          l = 33
+          l = 33,
+          fieldProcessed = "field_proc"
         ))
     ).asJavaMap
 
     val procOptExpected = Map(
-      "fieldRaw" -> "field raw",
-      "fieldProcessed" -> "field processed",
+      "fieldRaw" -> "field_raw",
       "dimension" -> 222,
-      "discardRaw" -> true,
       "exact" -> null,
+      "vectorType" -> VECTOR_TYPE_DOUBLE.value,
       "lsh" -> Map(
         "seed" -> 99L,
+        "fieldProcessed" -> "field_proc",
         "distance" -> DISTANCE_ANGULAR.index,
         "k" -> 22,
         "l" -> 33
