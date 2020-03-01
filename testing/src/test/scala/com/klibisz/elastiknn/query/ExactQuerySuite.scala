779cfe3de8ea33b562025d56b0116778412bc26a
Alex Klibisz <aklibisz@gmail.com>
2020-03-01 16:50:48 -0500

    Got ExactQuerySuite compiling

--- testing/src/test/scala/com/klibisz/elastiknn/query/ExactQuerySuite.scala
+++ testing/src/test/scala/com/klibisz/elastiknn/query/ExactQuerySuite.scala
@@ -1,76 +1,76 @@
-//package com.klibisz.elastiknn.query
-//
-//import com.klibisz.elastiknn.KNearestNeighborsQuery._
-//import com.klibisz.elastiknn.ProcessorOptions._
-//import com.klibisz.elastiknn.Similarity.SIMILARITY_JACCARD
-//import com.klibisz.elastiknn._
-//import org.scalatest._
-//
-///**
-//  * Tests for the exact query functionality, using test data generated via Python and scikit-learn.
-//  */
-//class ExactQuerySuite
-//    extends AsyncFunSuite
-//    with QuerySuite
-//    with Matchers
-//    with SilentMatchers
-//    with Inspectors
-//    with Elastic4sMatchers
-//    with ElasticAsyncClient {
-//
-//  private val fieldRaw = "vec_raw"
-//  private val fieldProc = "vec_proc"
-//
-//  for {
-//    sim <- Similarity.values
-//    dim <- testDataDims
-//    cache <- Seq(true, false)
-//  } yield {
-//
-//    val index: String = s"test-${sim.name.toLowerCase}-$dim"
-//    val pipelineId: String = s"$index-pipeline-${System.currentTimeMillis()}"
-//
-//    val harness = new Harness(sim, fieldRaw, dim, index, pipelineId, ExactComputedModelOptions(sim))
-//
-//    test(s"$dim, $cache, given") {
-//      harness.testGiven(ExactComputedQueryOptions(sim))
-//    }
-//
-//    val support = new Support("vec_raw", sim, dim, mopts)
-//
-//    test(s"$dim, $mopts, $cache, given") {
-//      support.testGiven(qoptsFunc, cache) { queriesAndResults =>
-//        forAll(queriesAndResults.silent) {
-//          case (query, res) =>
-//            res.hits.hits should have length query.similarities.length
-//            // Just check the similarity scores. Some vectors will have the same scores, so checking indexes is brittle.
-//            forAll(query.similarities.zip(res.hits.hits.map(_.score)).silent) {
-//              case (sim, score) => score shouldBe sim +- 1e-5f
-//            }
-//        }
-//      }
-//
-//    }
-//
-//    test(s"$dim, $mopts, $cache, indexed") {
-//      support.testIndexed(ExactComputedQueryOptions("vec_raw", sim), cache) { queriesAndResults =>
-//        forAll(queriesAndResults.silent) {
-//          case (query, id, res) =>
-//            val hits = res.hits.hits
-//            hits.length shouldBe >=(query.similarities.length)
-//            // The top hit should be the query vector itself.
-//            val self = hits.find(_.id == id)
-//            self shouldBe defined
-//            self.map(_.score) shouldBe Some(hits.map(_.score).max)
-//
-//            // The remaining hits have the right scores. Only consider the corpus vectors.
-//            val scores = hits.filter(_.id.startsWith(support.corpusVectorIdPrefix)).map(_.score).take(query.similarities.length)
-//            forAll(query.similarities.zip(scores).silent) {
-//              case (sim, score) => score shouldBe sim +- 1e-5f
-//            }
-//        }
-//      }
-//    }
-//  }
-//
-//}
+package com.klibisz.elastiknn.query
+
+import com.klibisz.elastiknn.KNearestNeighborsQuery._
+import com.klibisz.elastiknn.ProcessorOptions._
+import com.klibisz.elastiknn.Similarity.SIMILARITY_JACCARD
+import com.klibisz.elastiknn._
+import org.scalatest._
+
+/**
+  * Tests for the exact query functionality, using test data generated via Python and scikit-learn.
+  */
+class ExactQuerySuite
+    extends AsyncFunSuite
+    with QuerySuite
+    with Matchers
+    with SilentMatchers
+    with Inspectors
+    with Elastic4sMatchers
+    with ElasticAsyncClient {
+
+  private val fieldRaw = "vec_raw"
+  private val fieldProc = "vec_proc"
+
+  for {
+    sim <- Similarity.values
+    dim <- testDataDims
+    useCache <- Seq(true, false)
+    simToOptions: (Similarity => (ModelOptions, QueryOptions)) <- Seq(
+      (s: Similarity) =>
+        (ModelOptions.ExactComputed(ExactComputedModelOptions(s)), QueryOptions.ExactComputed(ExactComputedQueryOptions())),
+      (s: Similarity) =>
+        (ModelOptions.ExactIndexed(ExactIndexedModelOptions(s, fieldProc)), QueryOptions.ExactIndexed(ExactIndexedQueryOptions()))
+    )
+  } yield {
+
+    val index: String = s"test-${sim.name.toLowerCase}-$dim"
+    val pipelineId: String = s"$index-pipeline-${System.currentTimeMillis()}"
+
+    val (mopts, qopts) = simToOptions(sim)
+    val harness = new Harness(sim, fieldRaw, dim, index, pipelineId, mopts)
+
+    test(s"$mopts, $dim, given, $qopts, $useCache") {
+      harness.testGiven(qopts, useCache) { qAndR =>
+        forAll(qAndR.silent) {
+          case (query, res) =>
+            res.hits.hits should have length query.similarities.length
+            // Just check the similarity scores. Some vectors will have the same scores, so checking indexes is brittle.
+            forAll(query.similarities.zip(res.hits.hits.map(_.score)).silent) {
+              case (sim, score) => score shouldBe sim +- 1e-5f
+            }
+        }
+      }
+    }
+
+    test(s"$mopts, $dim, indexed, $qopts, $useCache") {
+      harness.testIndexed(qopts, useCache) { qAndR =>
+        forAll(qAndR.silent) {
+          case (query, id, res) =>
+            val hits = res.hits.hits
+            hits.length shouldBe >=(query.similarities.length)
+            // The top hit should be the query vector itself.
+            val self = hits.find(_.id == id)
+            self shouldBe defined
+            self.map(_.score) shouldBe Some(hits.map(_.score).max)
+
+            // The remaining hits have the right scores. Only consider the corpus vectors.
+            val scores = hits.filter(_.id.startsWith(harness.corpusVectorIdPrefix)).map(_.score).take(query.similarities.length)
+            forAll(query.similarities.zip(scores).silent) {
+              case (sim, score) => score shouldBe sim +- 1e-5f
+            }
+        }
+      }
+    }
+  }
+
+}
