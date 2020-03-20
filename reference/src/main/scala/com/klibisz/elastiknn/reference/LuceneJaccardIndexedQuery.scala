e4c7d96a90d2c9e53b8696d3b54d519c3fc6b4c0
Alex Klibisz <aklibisz@gmail.com>
2020-03-19 23:17:35 -0400

    LuceneJaccardIndexedQuery implemented with intersection from boolean query and stored num true

--- reference/src/main/scala/com/klibisz/elastiknn/reference/LuceneJaccardIndexedQuery.scala
+++ reference/src/main/scala/com/klibisz/elastiknn/reference/LuceneJaccardIndexedQuery.scala
@@ -1,79 +1,67 @@
 package com.klibisz.elastiknn.reference
 
 import java.io.File
-import java.nio.ByteBuffer
 import java.util
 import java.util.Objects
 
 import com.klibisz.elastiknn._
-import com.klibisz.elastiknn.{ElastiKnnVector, Similarity}
-import com.klibisz.elastiknn.Similarity.SIMILARITY_JACCARD
-import com.klibisz.elastiknn.models.ExactSimilarity
 import com.klibisz.elastiknn.utils.ElastiKnnVectorUtils
 import org.apache.lucene.analysis.core.WhitespaceAnalyzer
 import org.apache.lucene.codecs.simpletext.SimpleTextCodec
-import org.apache.lucene.document.{
-  BinaryDocValuesField,
-  Document,
-  Field,
-  FieldType,
-  IntRangeDocValuesField,
-  LongPoint,
-  NumericDocValuesField,
-  StoredField
-}
+import org.apache.lucene.document.{Document, Field, FieldType, StoredField}
 import org.apache.lucene.index._
 import org.apache.lucene.search._
+import org.apache.lucene.search.similarities.BooleanSimilarity
 import org.apache.lucene.store.MMapDirectory
-import org.apache.lucene.util.BytesRef
 
 object LuceneJaccardIndexedQuery extends ElastiKnnVectorUtils {
 
-  class JaccardIndexedQuery(val field: String, val sim: Similarity, val queryVector: ElastiKnnVector) extends Query {
+  object JaccardIndexedQuery {
+    def fieldTrueIndices(field: String): String = s"$field.true_indices"
+    def fieldNumTrue(field: String): String = s"$field.num_true"
+  }
 
-    private val existsQuery = new DocValuesFieldExistsQuery(field)
+  class JaccardIndexedQuery(val field: String, val queryVector: SparseBoolVector) extends Query {
+
+    import JaccardIndexedQuery._
+
+    private val booleanIntersectionQuery: BooleanQuery = {
+      val bqb = new BooleanQuery.Builder
+      queryVector.trueIndices.foreach { ti =>
+        bqb.add(new BooleanClause(new TermQuery(new Term(fieldTrueIndices(field), ti.toString)), BooleanClause.Occur.SHOULD))
+      }
+      bqb.build()
+    }
 
     class ExactSimWeight(searcher: IndexSearcher) extends Weight(this) {
 
-      private val existsWeight = existsQuery.createWeight(searcher, ScoreMode.COMPLETE_NO_SCORES, 1f)
+      searcher.setSimilarity(new BooleanSimilarity)
+
+      private val booleanWeight = booleanIntersectionQuery.createWeight(searcher, ScoreMode.COMPLETE, 1f)
 
       override def extractTerms(terms: util.Set[Term]): Unit = ()
 
       override def explain(context: LeafReaderContext, doc: Int): Explanation = ???
 
-      override def scorer(context: LeafReaderContext): Scorer = {
-        val existsScorer = existsWeight.scorer(context)
-        val existsIterator = if (existsScorer != null) existsScorer.iterator() else DocIdSetIterator.empty()
-        new ExactSimScorer(this, searcher, context, existsIterator)
-      }
+      override def scorer(context: LeafReaderContext): Scorer =
+        new ExactSimScorer(this, searcher, context, booleanWeight.scorer(context))
 
       override def isCacheable(ctx: LeafReaderContext): Boolean = false
     }
 
-    class ExactSimScorer(weight: Weight, searcher: IndexSearcher, context: LeafReaderContext, iterator: DocIdSetIterator)
+    class ExactSimScorer(weight: Weight, searcher: IndexSearcher, context: LeafReaderContext, booleanScorer: Scorer)
         extends Scorer(weight) {
 
-      private val reader = context.reader()
-      private val binaryDocValues = reader.getBinaryDocValues(field)
-
-      override def iterator(): DocIdSetIterator = iterator
+      override val iterator: DocIdSetIterator = if (booleanScorer != null) booleanScorer.iterator() else DocIdSetIterator.empty()
 
       override def getMaxScore(upTo: Int): Float = Float.MaxValue
 
       override def score(): Float = {
+        val intersection = booleanScorer.score()
         val docId = iterator.docID()
-        if (binaryDocValues.advanceExact(docId)) {
-          val bv = binaryDocValues.binaryValue()
-          val ekv = ElastiKnnVector.parseFrom(bv.bytes.take(bv.length))
-          val ekv2 = {
-            val bv2 = searcher.doc(docId).getField(field).binaryValue.bytes
-            ElastiKnnVector.parseFrom(bv2.take(bv2.length))
-          }
-          val numTrue = searcher.doc(docId).getField("num_true").numericValue.longValue
-          println((ekv, ekv2, numTrue))
-          val (score, _) = ExactSimilarity.apply(sim, queryVector, ekv).get
-          score.toFloat
-        } else 0f
+        val doc = searcher.doc(docId)
+        val numTrue = doc.getField(fieldNumTrue(field)).numericValue.intValue()
+        intersection / (queryVector.trueIndices.length + numTrue - intersection) * 1f
       }
 
       override def docID(): Int = iterator.docID()
@@ -82,14 +70,14 @@ object LuceneJaccardIndexedQuery extends ElastiKnnVectorUtils {
     override def createWeight(searcher: IndexSearcher, scoreMode: ScoreMode, boost: Float): Weight =
       new ExactSimWeight(searcher)
 
-    override def toString(field: String): String = s"$field:${sim.name}"
+    override def toString(field: String): String = s"$field:$queryVector"
 
     override def equals(other: Any): Boolean = other match {
-      case esq: JaccardIndexedQuery => esq.field == this.field && esq.sim == this.sim
+      case esq: JaccardIndexedQuery => esq.field == this.field && esq.queryVector == this.queryVector
       case _                        => false
     }
 
-    override def hashCode(): Int = Objects.hash(field, sim)
+    override def hashCode(): Int = Objects.hash(field, queryVector)
 
   }
 
@@ -105,13 +93,17 @@ object LuceneJaccardIndexedQuery extends ElastiKnnVectorUtils {
     idFieldType.setStored(true)
     idFieldType.setIndexOptions(IndexOptions.DOCS)
 
+    val trueIndicesFieldType = new FieldType()
+    trueIndicesFieldType.setIndexOptions(IndexOptions.DOCS)
+
     val field = "vec_raw"
-    val queryVector = ElastiKnnVector(ElastiKnnVector.Vector.SparseBoolVector(SparseBoolVector(Array(1, 2, 3), 10)))
+    val queryVector = SparseBoolVector(Array(1, 2, 3), 10)
     val sbvOpts = Seq(
       Some(SparseBoolVector(Array(1, 2, 3), 10)),
       None,
       Some(SparseBoolVector(Array(1, 3, 5), 10)),
-      Some(SparseBoolVector(Array(3, 6, 7, 8), 10))
+      Some(SparseBoolVector(Array(3, 6, 7, 8), 10)),
+      Some(SparseBoolVector(Array(1, 4, 5, 6, 7, 8, 9), 10))
     )
 
     sbvOpts.zipWithIndex.foreach {
@@ -119,10 +111,10 @@ object LuceneJaccardIndexedQuery extends ElastiKnnVectorUtils {
         val doc = new Document
         doc.add(new Field("id", i.toString, idFieldType))
         sbvOpt.foreach { sbv =>
-          val ekv = ElastiKnnVector(ElastiKnnVector.Vector.SparseBoolVector(sbv))
-          doc.add(new BinaryDocValuesField(field, new BytesRef(ekv.toByteArray)))
-          doc.add(new StoredField(field, ekv.toByteArray))
-          doc.add(new StoredField("num_true", sbv.trueIndices.length))
+          sbv.trueIndices.foreach { ti =>
+            doc.add(new Field(JaccardIndexedQuery.fieldTrueIndices(field), ti.toString, trueIndicesFieldType))
+          }
+          doc.add(new StoredField(JaccardIndexedQuery.fieldNumTrue(field), sbv.trueIndices.length))
         }
         ixWriter.addDocument(doc)
     }
@@ -134,7 +126,7 @@ object LuceneJaccardIndexedQuery extends ElastiKnnVectorUtils {
     val ixReader = DirectoryReader.open(ixDir)
     val ixSearcher = new IndexSearcher(ixReader)
 
-    val topDocs = ixSearcher.search(new JaccardIndexedQuery(field, SIMILARITY_JACCARD, queryVector), 10)
+    val topDocs = ixSearcher.search(new JaccardIndexedQuery(field, queryVector), 10)
 
     println(s"Found ${topDocs.scoreDocs.length} docs")
     topDocs.scoreDocs.foreach(d => println(s"${d.doc}, ${d.score}, ${ixSearcher.doc(d.doc)}"))
