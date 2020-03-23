07eae27b17005aa564ae0118a0689b32c236841e
Alex Klibisz <aklibisz@gmail.com>
2020-03-23 15:45:38 -0400

    Got it working by ensuring that the Mapping Json can translate back into an XContentBuilder

--- es74x/src/main/scala/com/klibisz/elastiknn/mapper/VectorMapper.scala
+++ es74x/src/main/scala/com/klibisz/elastiknn/mapper/VectorMapper.scala
@@ -1,19 +1,21 @@
 package com.klibisz.elastiknn.mapper
 
+import java.io.ByteArrayInputStream
 import java.util
 
 import com.klibisz.elastiknn.{ELASTIKNN_NAME, api}
 import com.klibisz.elastiknn.api.Mapping
 import io.circe.syntax._
-import io.circe.{Decoder, Encoder}
+import io.circe.{Decoder, Encoder, Json, JsonObject}
 import org.apache.lucene.index.IndexableField
 import org.apache.lucene.search.{DocValuesFieldExistsQuery, Query}
-import org.elasticsearch.common.xcontent.{ToXContent, XContentBuilder}
+import org.elasticsearch.common.xcontent.{ToXContent, XContentBuilder, XContentType}
 import org.elasticsearch.index.mapper.Mapper.TypeParser
 import org.elasticsearch.index.mapper._
 import org.elasticsearch.index.query.QueryShardContext
 
-import scala.util.Failure
+import scala.collection.JavaConverters._
+import scala.util.{Failure, Success, Try}
 
 object VectorMapper {
   val sparseBoolVector: VectorMapper[api.Mapping.ElastiknnSparseBoolVector] =
@@ -42,68 +44,70 @@ abstract class VectorMapper[M <: api.Mapping](val CONTENT_TYPE: String) {
     import com.klibisz.elastiknn.utils.CirceUtils.javaMapEncoder
 
     override def parse(name: String, node: util.Map[String, AnyRef], parserContext: TypeParser.ParserContext): Mapper.Builder[_, _] = {
-      val builder = new Builder(name)
-      TypeParsers.parseField(builder, name, node, parserContext)
-      implicitly[Decoder[Mapping]]
+      val mapping = implicitly[Decoder[Mapping]]
         .decodeJson(node.asJson)
         .map(_.asInstanceOf[M])
-        .foreach(builder.mapping = _)
+        .toTry
       node.clear()
-//      if (builder.mapping != null) {
-//        implicitly[Encoder[Mapping]]
-//          .apply(builder.mapping)
-//          .asObject
-//          .foreach(_.keys.foreach(node.remove))
-//      }
+      val builder = new Builder(name, mapping)
+//      TypeParsers.parseField(builder, name, node, parserContext)
       builder
     }
   }
 
-  private class Builder(name: String) extends FieldMapper.Builder[Builder, FieldMapper](name, fieldType, fieldType) {
+  private class Builder(name: String, mapping: Try[M]) extends FieldMapper.Builder[Builder, FieldMapper](name, fieldType, fieldType) {
 
-    var mapping: M = _
+    private def populateBuilder(json: Json, builder: XContentBuilder, skipKeys: Set[String] = Set.empty): Unit = {
+      def populate(json: Json): Unit =
+        if (json.isBoolean) json.asBoolean.foreach(builder.value)
+        else if (json.isString) json.asString.foreach(builder.value)
+        else if (json.isNumber) json.asNumber.foreach { n =>
+          lazy val asInt = n.toInt
+          lazy val asLong = n.toLong
+          if (asInt.isDefined) builder.value(asInt.get)
+          else if (asLong.isDefined) builder.value(asLong.get)
+          else builder.value(n.toDouble)
+        } else if (json.isArray) json.asArray.foreach(_.foreach(populate))
+        else if (json.isObject) json.asObject.foreach {
+          _.toIterable.foreach {
+            case (k, v) =>
+              if (v.isObject) {
+                builder.startObject(k)
+                populate(v)
+                builder.endObject()
+              } else if (v.isArray) {
+                builder.startArray(k)
+                populate(v)
+                builder.endArray()
+              } else {
+                builder.field(k)
+                populate(v)
+              }
+          }
+        }
+      populate(json)
+    }
 
     override def build(context: Mapper.BuilderContext): FieldMapper = {
       super.setupFieldType(context)
 
       new FieldMapper(name, fieldType, defaultFieldType, context.indexSettings(), multiFieldsBuilder.build(this, context), copyTo) {
-        override def parse(context: ParseContext): Unit = mapper.parse(context, mapping)
+        override def parse(context: ParseContext): Unit = mapper.parse(context, mapping.get)
         override def parseCreateField(context: ParseContext, fields: util.List[IndexableField]): Unit =
           throw new IllegalStateException("parse() is implemented directly")
         override def contentType(): String = CONTENT_TYPE
         override def doXContentBody(builder: XContentBuilder, includeDefaults: Boolean, params: ToXContent.Params): Unit = {
           super.doXContentBody(builder, includeDefaults, params)
+          print(mapping)
+          mapping.toOption
+            .map(implicitly[Encoder[Mapping]].apply)
+            .flatMap(_.asObject)
+            .map(_.toIterable.filter(_._1 != "type"))
+            .map(JsonObject.fromIterable)
+            .map(Json.fromJsonObject)
+            .foreach(populateBuilder(_, builder))
 
-          builder.field("dims", 100)
-          builder.field("model_options", new util.HashMap[String, AnyRef] {
-            put("type", "jaccard_lsh")
-            put("bands", 100.asInstanceOf[AnyRef])
-            put("rows", 1.asInstanceOf[AnyRef])
-          })
-
-//          if (mapping != null) {
-//            implicitly[Encoder[Mapping]]
-//              .apply(mapping)
-//              .asObject
-//              .foreach(_.toIterable.foreach {
-//                case (k, v) => if (k != "type") ()
-//              })
-//          }
-
-          // builder.field("wtf", 99)
-//          mapping match {
-//            case Mapping.ElastiknnSparseBoolVector(dims, _) =>
-////              builder.field("dims", dims)
-////              builder.field("wtf", 99)
-//            case Mapping.ElastiknnDenseFloatVector(dims, _) =>
-////              builder.field("dims", dims)
-////              builder.field("wtf", 99)
-//            case _ => ()
-//          }
         }
-
-//        override def doXContentDocValues(builder: XContentBuilder, includeDefaults: Boolean): Unit =
-//          super.doXContentDocValues(builder, includeDefaults)
       }
     }
   }
