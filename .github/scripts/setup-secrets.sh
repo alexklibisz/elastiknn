793cb5232be04e47b2c0f6285bb7f13775c7e21e
Alex Klibisz <aklibisz@gmail.com>
2020-04-09 00:54:15 -0400

    ssh secrets and releasing docs

--- .github/scripts/setup-secrets.sh
+++ .github/scripts/setup-secrets.sh
@@ -6,3 +6,6 @@ mkdir -p ~/.gnupg ~/.gradle
 echo $GPG_SECRET_B64 | base64 --decode > ~/.gnupg/secring.gpg
 echo $GRADLE_PROPERTIES_B64 | base64 --decode > ~/.gradle/gradle.properties
 echo $PYPIRC_B64 | base64 --decode > ~/.pypirc
+echo $SSH_CONFIG_B64 | base64 --decode > ~/.ssh/config
+echo $SSH_IDRSA_B64 | base64 --decode > ~/.ssh/id_rsa
+chmod 400 ~/.ssh/id_rsa
