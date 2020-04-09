92391a757efbdcc5f259c3cf34a8e3e3add3eac4
Alex Klibisz <aklibisz@gmail.com>
2020-04-09 01:26:50 -0400

    iterating

--- .github/scripts/setup-secrets.sh
+++ .github/scripts/setup-secrets.sh
@@ -10,7 +10,3 @@ echo $GRADLE_PROPERTIES_B64 | base64 --decode > $HOME/.gradle/gradle.properties
 echo $PYPIRC_B64 | base64 --decode > $HOME/.pypirc
 echo $SSH_CONFIG_B64 | base64 --decode > $HOME/.ssh/config
 echo $SSH_IDRSA_B64 | base64 --decode > $HOME/.ssh/elastiknn-site
-
-ssh-keyscan server119.web-hosting.com >> $HOME/.ssh/known_hosts
-cat $HOME/.ssh/known_hosts
-# ssh elastiknn-site ls
