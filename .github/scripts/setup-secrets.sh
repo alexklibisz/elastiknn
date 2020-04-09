4f28c0e11dc8dc918af4e7034a353b52c67b3210
Alex Klibisz <aklibisz@gmail.com>
2020-04-09 01:22:49 -0400

    iterating

--- .github/scripts/setup-secrets.sh
+++ .github/scripts/setup-secrets.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-mkdir -p ~/.gnupg ~/.gradle ~/.ssh
+mkdir -p $HOME/.gnupg $HOME/.gradle $HOME/.ssh
 
-echo $GPG_SECRET_B64 | base64 --decode > ~/.gnupg/secring.gpg
-echo $GRADLE_PROPERTIES_B64 | base64 --decode > ~/.gradle/gradle.properties
-echo $PYPIRC_B64 | base64 --decode > ~/.pypirc
-echo $SSH_CONFIG_B64 | base64 --decode > ~/.ssh/config
-echo $SSH_IDRSA_B64 | base64 --decode > ~/.ssh/elastiknn-site
+echo $GPG_SECRET_B64 | base64 --decode > $HOME/.gnupg/secring.gpg
+echo $GRADLE_PROPERTIES_B64 | base64 --decode > $HOME/.gradle/gradle.properties
+echo $PYPIRC_B64 | base64 --decode > $HOME/.pypirc
+echo $SSH_CONFIG_B64 | base64 --decode > $HOME/.ssh/config
+echo $SSH_IDRSA_B64 | base64 --decode > $HOME/.ssh/elastiknn-site
 
-ssh-keyscan server119.web-hosting.com >> ~/.ssh/known_hosts
-ssh elastiknn-site ls
+ssh-keyscan server119.web-hosting.com >> $HOME/.ssh/known_hosts
+# ssh elastiknn-site ls
