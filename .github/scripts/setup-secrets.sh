e7ec6926f18e21ce9b41c349247dcddf11abe674
Alex Klibisz <aklibisz@gmail.com>
2020-04-09 01:06:21 -0400

    iterating

--- .github/scripts/setup-secrets.sh
+++ .github/scripts/setup-secrets.sh
@@ -8,4 +8,5 @@ echo $GRADLE_PROPERTIES_B64 | base64 --decode > ~/.gradle/gradle.properties
 echo $PYPIRC_B64 | base64 --decode > ~/.pypirc
 echo $SSH_CONFIG_B64 | base64 --decode > ~/.ssh/config
 echo $SSH_IDRSA_B64 | base64 --decode > ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa | md5sum
 chmod 400 ~/.ssh/id_rsa
