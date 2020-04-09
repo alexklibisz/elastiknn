e60f4aff75b2d37dcd6376b9c3750cbb15e1e38b
Alex Klibisz <aklibisz@gmail.com>
2020-04-09 01:35:52 -0400

    permissions

--- .github/scripts/setup-secrets.sh
+++ .github/scripts/setup-secrets.sh
@@ -10,3 +10,4 @@ echo $GRADLE_PROPERTIES_B64 | base64 --decode > $HOME/.gradle/gradle.properties
 echo $PYPIRC_B64 | base64 --decode > $HOME/.pypirc
 echo $SSH_CONFIG_B64 | base64 --decode > $HOME/.ssh/config
 echo $SSH_IDRSA_B64 | base64 --decode > $HOME/.ssh/elastiknn-site
+chmod 400 $HOME/.ssh/elastiknn-site
