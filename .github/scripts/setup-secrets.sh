22588874c096f91feb77f492ab53e8b146b7403e
Alex Klibisz <aklibisz@gmail.com>
2020-04-09 00:57:39 -0400

    more fix

--- .github/scripts/setup-secrets.sh
+++ .github/scripts/setup-secrets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-mkdir -p ~/.gnupg ~/.gradle
+mkdir -p ~/.gnupg ~/.gradle ~/.ssh
 
 echo $GPG_SECRET_B64 | base64 --decode > ~/.gnupg/secring.gpg
 echo $GRADLE_PROPERTIES_B64 | base64 --decode > ~/.gradle/gradle.properties
