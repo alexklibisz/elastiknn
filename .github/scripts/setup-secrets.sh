33957cc7294fe8a7558c94f73e765e7c9c546118
Alex Klibisz <aklibisz@gmail.com>
2020-04-09 01:14:35 -0400

    iterating

--- .github/scripts/setup-secrets.sh
+++ .github/scripts/setup-secrets.sh
@@ -8,7 +8,7 @@ echo $GRADLE_PROPERTIES_B64 | base64 --decode > ~/.gradle/gradle.properties
 echo $PYPIRC_B64 | base64 --decode > ~/.pypirc
 echo $SSH_CONFIG_B64 | base64 --decode > ~/.ssh/config
 echo $SSH_IDRSA_B64 | base64 --decode > ~/.ssh/elastiknn-site
-chmod 400 ~/.ssh/id_rsa
-cat ~/.ssh/id_rsa | md5sum
+chmod 400 ~/.ssh/elastiknn-site
+cat ~/.ssh/elastiknn-site | md5sum
 cat ~/.ssh/config
 ls -la ~/.ssh
