fccbc19b4e66871dcaae387db0e52c1bf78b7c93
Alex Klibisz <aklibisz@gmail.com>
2020-05-04 00:02:40 -0400

    Should just need to implement runSingle method now

--- benchmarks/src/main/scala/com/klibisz/elastiknn/benchmarks/Driver.scala
+++ benchmarks/src/main/scala/com/klibisz/elastiknn/benchmarks/Driver.scala
@@ -15,29 +15,37 @@ object Driver extends App {
                      elasticsearchHost: String = "http://localhost",
                      elasticsearchPort: Int = 9200,
                      resultsFile: File = new File("/tmp/results.json"),
-                     ks: Seq[Int] = Seq(10, 100))
+                     ks: Seq[Int] = Seq(10, 100),
+                     testQueries: Int = 1000)
 
   private val optionParser = new scopt.OptionParser[Options]("Benchmarks driver") {
     opt[String]('d', "datasetsDirectory").action((s, c) => c.copy(datasetsDirectory = new File(s)))
     opt[String]('h', "elasticsearchHost").action((s, c) => c.copy(elasticsearchHost = s))
     opt[Int]('p', "elasticsearchPort").action((i, c) => c.copy(elasticsearchPort = i))
     opt[String]('o', "resultsFile").action((s, c) => c.copy(resultsFile = new File(s)))
+    opt[Int]('q', "testQueries").action((i, c) => c.copy(testQueries = i))
   }
+
+  private val vectorField: String = "vec"
+  private val querySelectionSeed: Long = 0
 
   private case class SearchResult(id: Int, neighborIds: Vector[Int], duration: Duration)
 
-  private def computeResult(exact: Vector[SearchResult], test: Vector[SearchResult]): Result = ???
-
-  private val vectorField: String = "vec"
+  private def recalls(exact: Vector[SearchResult], test: Vector[SearchResult]): Seq[Double] = exact.zip(test).map {
+    case (ex, ts) => ex.neighborIds.intersect(ts.neighborIds).length * 1d / ex.neighborIds.length
+  }
 
   private def runSingle(dataset: Dataset,
                         mapping: Mapping,
                         mkQuery: (String, Vec, Int) => NearestNeighborsQuery,
-                        k: Int): ZIO[ElastiknnZioClient with DatasetClient, Nothing, Vector[SearchResult]] =
+                        k: Int,
+                        testQueries: Int): ZIO[ElastiknnZioClient with DatasetClient, Nothing, Vector[SearchResult]] =
+    // Index the dataset, hold out `testQueries` vectors to use as queries, run the queries, return one SearchResult for each query.
     ZIO.succeed(Vector.empty)
 
   private def runExperiments(experiments: Seq[Experiment],
-                             ks: Seq[Int]): ZIO[Console with DatasetClient with ResultClient with ElastiknnZioClient, Throwable, Unit] =
+                             ks: Seq[Int],
+                             testQueries: Int = 1000): ZIO[DatasetClient with ResultClient with ElastiknnZioClient, Throwable, Unit] =
     for {
       resultClient: ResultClient.Service <- ZIO.access[ResultClient](_.get)
       _ <- ZIO.collectAll(
@@ -46,21 +54,23 @@ object Driver extends App {
           k <- ks
         } yield
           for {
-            exactResults <- runSingle(exp.dataset, exp.exact.mapping, exp.exact.mkQuery.head, k)
-            _ <- ZIO.collectAll(
+            exactResults <- runSingle(exp.dataset, exp.exact.mapping, exp.exact.mkQuery.head, k, testQueries)
+            _ <- ZIO.collectAll(for {
+              maq <- exp.maqs
+              mkQuery <- maq.mkQuery
+              emptyResult = Result(exp.dataset, exp.exact.mapping, mkQuery(vectorField, Vec.Empty(), k), k, Seq.empty, Seq.empty)
+            } yield
               for {
-                maq <- exp.maqs
-                mkQuery <- maq.mkQuery
-              } yield
-                for {
-                  found <- resultClient.find(exp.dataset, exp.exact.mapping, mkQuery(vectorField, Vec.Empty(), k), k)
-                  _ <- if (found.isDefined) ZIO.succeed(())
-                  else
-                    for {
-                      testResults <- runSingle(exp.dataset, maq.mapping, mkQuery, k)
-                      _ <- resultClient.save(computeResult(exactResults, testResults))
-                    } yield ()
-                } yield ())
+                found <- resultClient.find(emptyResult.dataset, emptyResult.mapping, emptyResult.query, emptyResult.k)
+                _ <- if (found.isDefined) ZIO.succeed(())
+                else
+                  for {
+                    testResults <- runSingle(exp.dataset, maq.mapping, mkQuery, k, testQueries)
+                    populatedResult: Result = emptyResult.copy(durations = testResults.map(_.duration.toMillis),
+                                                               recalls = recalls(exactResults, testResults))
+                    _ <- resultClient.save(populatedResult)
+                  } yield ()
+              } yield ())
           } yield ()
       )
     } yield ()
@@ -72,7 +82,7 @@ object Driver extends App {
         val layer: ZLayer[Any, Throwable, Console with DatasetClient with ResultClient with ElastiknnZioClient] =
           Console.live ++ DatasetClient.local(opts.datasetsDirectory) ++ ResultClient.local(opts.resultsFile) ++
             ElastiknnZioClient.fromFutureClient(opts.elasticsearchHost, opts.elasticsearchPort, strictFailure = true)
-        runExperiments(Seq(Experiment.hamming(Dataset.AmazonHomePhash)), opts.ks)
+        runExperiments(Experiment.defaults, opts.ks, opts.testQueries)
           .provideLayer(layer)
           .mapError(System.err.println)
           .fold(_ => 1, _ => 0)
