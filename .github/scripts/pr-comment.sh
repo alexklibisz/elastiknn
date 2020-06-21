5f59f80c31647586d84ddd5b67b7f916bcd4b596
Alex Klibisz <aklibisz@gmail.com>
2020-06-20 22:10:46 -0400

    only check open prs

--- .github/scripts/pr-comment.sh
+++ .github/scripts/pr-comment.sh
@@ -3,7 +3,7 @@ set -e
 
 BODY=$(cat $1)
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-NUMS=$(hub pr list -f '%I %H%n' | grep $BRANCH | cut -d' ' -f1 || true)
+NUMS=$(hub pr list -s open -f '%I %H%n' | grep $BRANCH | cut -d' ' -f1 || true)
 DATA=$(jq --arg body "$BODY" '{"body": $body}' <<< {})
 
 for N in $NUMS;
