5d269646e0b45bfff1491fd51514bb7921c062fc
Alex Klibisz <aklibisz@gmail.com>
2020-07-29 20:43:08 -0400

    Aws cli

--- .github/scripts/setup-env.sh
+++ .github/scripts/setup-env.sh
@@ -9,3 +9,4 @@ echo $PYPIRC_B64 | base64 --decode > $HOME/.pypirc
 echo $SSH_CONFIG_B64 | base64 --decode > $HOME/.ssh/config
 echo $SSH_IDRSA_B64 | base64 --decode > $HOME/.ssh/elastiknn-site
 chmod 400 $HOME/.ssh/elastiknn-site
+envsubst < .github/scripts/aws-credentials-template > ~/.aws/credentials
