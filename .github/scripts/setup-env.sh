655e9001ebb7e0b0c3f6c27eb1c6f39c74f56c7f
Alex Klibisz <aklibisz@gmail.com>
2020-07-29 20:44:46 -0400

    Aws cli

--- .github/scripts/setup-env.sh
+++ .github/scripts/setup-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-mkdir -p $HOME/.gnupg $HOME/.gradle $HOME/.ssh
+mkdir -p $HOME/.gnupg $HOME/.gradle $HOME/.ssh $HOME/.aws
 
 echo $GPG_SECRET_B64 | base64 --decode > $HOME/.gnupg/secring.gpg
 echo $GRADLE_PROPERTIES_B64 | base64 --decode > $HOME/.gradle/gradle.properties
@@ -9,4 +9,4 @@ echo $PYPIRC_B64 | base64 --decode > $HOME/.pypirc
 echo $SSH_CONFIG_B64 | base64 --decode > $HOME/.ssh/config
 echo $SSH_IDRSA_B64 | base64 --decode > $HOME/.ssh/elastiknn-site
 chmod 400 $HOME/.ssh/elastiknn-site
-envsubst < .github/scripts/aws-credentials-template > ~/.aws/credentials
+envsubst < .github/scripts/aws-credentials-template > $HOME/.aws/credentials
