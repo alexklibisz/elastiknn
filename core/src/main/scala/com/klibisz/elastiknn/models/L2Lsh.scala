9f8f8be0f1ac42b7632b5739244491d5498b42d4
Alex Klibisz <aklibisz@gmail.com>
2020-07-31 22:00:26 -0400

    first pass implementation compiles

--- core/src/main/scala/com/klibisz/elastiknn/models/L2Lsh.scala
+++ core/src/main/scala/com/klibisz/elastiknn/models/L2Lsh.scala
@@ -1,10 +1,14 @@
 package com.klibisz.elastiknn.models
 
+import java.util
+
+import com.google.common.collect.{MinMaxPriorityQueue, Sets}
 import com.klibisz.elastiknn.api.{Mapping, Vec}
 import com.klibisz.elastiknn.storage.StoredVec
 import com.klibisz.elastiknn.storage.UnsafeSerialization.writeInt
 
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.JavaConverters._
 import scala.util.Random
 
 /**
@@ -21,32 +25,98 @@ import scala.util.Random
   */
 final class L2Lsh(override val mapping: Mapping.L2Lsh) extends HashingFunction[Mapping.L2Lsh, Vec.DenseFloat, StoredVec.DenseFloat] {
 
+  private def cfor(i: Int)(pred: Int => Boolean, inc: Int => Int)(f: Int => Unit): Unit = {
+    var i_ = i
+    while (pred(i_)) {
+      f(i_)
+      i_ = inc(i_)
+    }
+  }
+
   import mapping._
   private implicit val rng: Random = new Random(0)
   private val hashVecs: Array[Vec.DenseFloat] = (0 until (L * k)).map(_ => Vec.DenseFloat.random(dims)).toArray
   private val biases: Array[Float] = (0 until (L * k)).map(_ => rng.nextFloat() * r).toArray
 
+  // Pre-compute the 3^k perturbations. ({-1, 0, 1}, {-1, 0, 1}, ... {-1, 0, 1}) k times.
+  private val perturbations: Array[Array[Short]] = {
+    val deltas: util.Set[Short] = Set(-1, 0, 1).map(_.toShort).asJava
+    Sets
+      .cartesianProduct((0 until k).map(_ => deltas): _*)
+      .asScala
+      .toArray
+      .map(_.asScala.toArray)
+      .sortBy(_.mkString(","))
+  }
+  private val zeroPerturbation: Array[Short] = Array(-1, 0, 1)
+
   override def apply(v: Vec.DenseFloat): Array[HashAndFreq] = hashWithProbes(v, 0)
 
-  def hashWithProbes(v: Vec.DenseFloat, probes: Int): Array[HashAndFreq] = {
-    val hashes = new Array[HashAndFreq](L)
-    var ixHashes = 0
-    var ixHashVecs = 0
-    while (ixHashes < L) {
-      val lBarr = writeInt(ixHashes)
-      val hashBuf = new ArrayBuffer[Byte](lBarr.length + k * 4)
-      hashBuf.appendAll(lBarr)
-      var ixRows = 0
-      while (ixRows < k) {
-        val hash = math.floor((hashVecs(ixHashVecs).dot(v) + biases(ixHashVecs)) / r).toInt
-        hashBuf.appendAll(writeInt(hash))
-        ixRows += 1
-        ixHashVecs += 1
-      }
-      hashes.update(ixHashes, HashAndFreq.once(hashBuf.toArray))
-      ixHashes += 1
+  private def scorePerturbation(projections: Array[Float], hashes: Array[Int], perturbation: Array[Short]): Float = {
+    var score = 0f
+    cfor(0)(_ < k, _ + 1) { ixK =>
+      val xi =
+        if (perturbation(ixK) == 0) 0
+        else {
+          val neg = projections(ixK) - hashes(ixK) * r
+          if (perturbation(ixK) == -1) neg
+          else r - neg
+        }
+      score += xi * xi
     }
-    hashes
+    score
+  }
+
+  def hashWithProbes(v: Vec.DenseFloat, probes: Int): Array[HashAndFreq] = {
+    val allHashes = new Array[HashAndFreq](L * (probes + 1))
+
+    cfor(0)(_ < L, _ + 1) { ixL =>
+      // Each hash generated for this table is prefixed with these bytes.
+      val lBarr = writeInt(ixL)
+
+      // Project and hash the vector onto the next k random vectors.
+      // Need both projections and hashes to compute perturbation scores.
+      val (projections, hashes): (Array[Float], Array[Int]) = {
+        val parr = new Array[Float](k)
+        val harr = new Array[Int](k)
+        cfor(0)(_ < k, _ + 1) { ixK =>
+          val p = hashVecs(ixL * k + ixK).dot(v) + biases(ixL * k + ixK)
+          parr.update(ixK, p)
+          harr.update(ixK, math.floor(p / r).toInt)
+        }
+        (parr, harr)
+      }
+
+      // Sort the perturbations by their score.
+      val perturbationHeap = MinMaxPriorityQueue
+        .orderedBy((p1: Array[Short], p2: Array[Short]) => {
+          Ordering.Float.compare(
+            scorePerturbation(projections, hashes, p1),
+            scorePerturbation(projections, hashes, p2)
+          )
+        })
+        .maximumSize(probes + 1)
+        .create[Array[Short]]()
+
+      // Special case for zero probes.
+      if (probes == 0) perturbationHeap.add(zeroPerturbation)
+      else perturbations.foreach(perturbationHeap.add)
+
+      var ixProbes = 0
+      while (!perturbationHeap.isEmpty) {
+        val hashBuf = new ArrayBuffer[Byte](lBarr.length + k * 4)
+        hashBuf.appendAll(lBarr)
+        val perturbation = perturbationHeap.removeFirst()
+        cfor(0)(_ < k, _ + 1) { ixK =>
+          hashBuf.appendAll(writeInt(hashes(ixK) + perturbation(ixK)))
+        }
+        allHashes.update(ixL + ixProbes, HashAndFreq.once(hashBuf.toArray))
+        ixProbes += 1
+      }
+
+    }
+
+    allHashes
   }
 
   //  private[models] val pertSets: Array[Array[Int]] = {
@@ -92,5 +162,24 @@ final class L2Lsh(override val mapping: Mapping.L2Lsh) extends HashingFunction[M
   //
   //    ???
   //  }
+
+}
+
+object CartesianProduct extends App {
+
+  import scala.collection.JavaConverters._
+
+  val k = 3
+  val perturbations: Array[Array[Int]] = {
+    val coords = Set(-1, 0, 1).asJava
+    Sets
+      .cartesianProduct((0 until k).map(_ => coords): _*)
+      .asScala
+      .toArray
+      .map(_.asScala.toArray)
+      .sortBy(_.mkString(","))
+  }
+
+  perturbations.map(_.toSeq).foreach(println)
 
 }
