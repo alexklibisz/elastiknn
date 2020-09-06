02fb06fb7aeb5eb4fc0f47c6394d2b24a2403757
Alex Klibisz <aklibisz@gmail.com>
2020-09-06 16:28:00 -0400

    Fixed pareto computation in analyze.py

--- elastiknn-benchmarks/python/analyze.py
+++ elastiknn-benchmarks/python/analyze.py
@@ -1,25 +1,19 @@
 import itertools
 import json
-from typing import List
-
-from bokeh.layouts import column
-from bokeh.models import Legend
-from bokeh.plotting import ColumnDataSource, figure, output_file, save
-from bokeh.palettes import Dark2_5 as palette
 
 import pandas as pd
-import pandasql as ps
+from bokeh.layouts import column
+from bokeh.models import Legend
+from bokeh.palettes import Dark2_5 as palette
+from bokeh.plotting import ColumnDataSource, figure, output_file, save
+from pareto import eps_sort
 
 
-# def pareto(df: pd.DataFrame, colx: str, coly: str)
-
-def pareto_frontier(df: pd.DataFrame, colx: str, coly: str, round_to: int = 2) -> pd.DataFrame:
-    """Return the subset of the given dataframe comprising the pareto frontier."""
-    # For some reason this type of aggregation is abysmally difficult with pure pandas.
-    cols = ",".join(filter(lambda c: c not in {colx, coly}, df.columns))
-    q = f"select {cols}, {colx}, max({coly}) as {coly} from df group by(round({colx}, {round_to}))"
-    x = ps.sqldf(q, dict(df=df))
-    return x
+def pareto_frontier(df: pd.DataFrame, colx: str, coly: str) -> pd.DataFrame:
+    cxi = list(df.columns).index(colx)
+    cyi = list(df.columns).index(coly)
+    rows = eps_sort([list(df.itertuples(False))], objectives=[cxi, cyi], maximize=[cxi, cyi])
+    return pd.DataFrame(rows, columns=df.columns).sort_values([colx])
 
 
 def cleanup_query(s: str) -> str:
@@ -33,35 +27,39 @@ def cleanup_mapping(s: str) -> str:
 
 
 def main():
-    df = pd.read_csv('/home/alex/Desktop/results-1597606608/aggregate.csv')
+    df = pd.read_csv('/home/alex/Desktop/aggregate.csv')
     output_file("results.html", title="Elastiknn Benchmark Results")
     colors = itertools.cycle(palette)
-    tooltips = [("mapping", "@mapping"), ("query", "@query")]
+    tooltips = [("mapping", "@mapping"), ("query", "@query"), ("recall", "@recall"), ("queries", "@queries")]
     xcol = "recall"
     ycol = "queriesPerSecond"
     figures = []
     for (dataset, g1) in df.groupby('dataset'):
         print(dataset)
         p = figure(plot_width=1600, plot_height=800, title=dataset, tooltips=tooltips,
-                   x_axis_label="Average Recall", y_axis_label="Queries / Second / Shard",
+                   x_axis_label="Average Recall", y_axis_label="Queries/Second",
                    toolbar_location="above")
         legend_items = []
         for ((algorithm, k), g2) in g1.groupby(['algorithm', 'k']):
             print(algorithm, k)
             legend_label = f"{algorithm}, {k}"
             color = next(colors)
-            pareto = pareto_frontier(g2, xcol, ycol, 2)
+            pareto = pareto_frontier(g2, xcol, ycol)
             source_full = ColumnDataSource(data=dict(
                 x=g2[xcol],
                 y=g2[ycol].values,
                 mapping=list(map(cleanup_mapping, g2['mapping'])),
-                query=list(map(cleanup_query, g2['query']))
+                query=list(map(cleanup_query, g2['query'])),
+                recall=list(g2['recall']),
+                queries=list(g2['queriesPerSecond'])
             ))
             source_pareto = ColumnDataSource(data=dict(
                 x=pareto[xcol],
                 y=pareto[ycol].values,
                 mapping=list(map(cleanup_mapping, pareto['mapping'])),
-                query=list(map(cleanup_query, pareto['query']))
+                query=list(map(cleanup_query, pareto['query'])),
+                recall=list(pareto['recall']),
+                queries=list(pareto['queriesPerSecond'])
             ))
             legend_items += [(
                 legend_label,
