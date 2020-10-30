540a12b76d745258387b6227323ccc379ff943a7
Alex Klibisz <aklibisz@gmail.com>
2020-10-29 20:49:56 -0400

    Using Lucene70DocValuesFormat gets speed back to normal!

--- elastiknn-plugin/src/main/scala/com/klibisz/elastiknn/ElastiknnPlugin.scala
+++ elastiknn-plugin/src/main/scala/com/klibisz/elastiknn/ElastiknnPlugin.scala
@@ -1,15 +1,19 @@
 package com.klibisz.elastiknn
 
-import java.util
+import java.{lang, util}
+import java.util.Optional
 
+import com.klibisz.elastiknn.codec.ElastiknnCodecService
 import com.klibisz.elastiknn.mapper.VectorMapper
 import com.klibisz.elastiknn.query._
-import org.elasticsearch.common.settings.Settings
+import org.elasticsearch.common.settings.{Setting, Settings}
+import org.elasticsearch.index.IndexSettings
+import org.elasticsearch.index.engine.{Engine, EngineConfig, EngineFactory, InternalEngine}
 import org.elasticsearch.index.mapper.Mapper
 import org.elasticsearch.plugins.SearchPlugin.QuerySpec
 import org.elasticsearch.plugins._
 
-class ElastiknnPlugin(settings: Settings) extends Plugin with IngestPlugin with SearchPlugin with ActionPlugin with MapperPlugin {
+class ElastiknnPlugin(settings: Settings) extends Plugin with IngestPlugin with SearchPlugin with MapperPlugin with EnginePlugin {
 
   override def getQueries: util.List[SearchPlugin.QuerySpec[_]] = util.Arrays.asList(
     new QuerySpec(KnnQueryBuilder.NAME, KnnQueryBuilder.Reader, KnnQueryBuilder.Parser)
@@ -21,6 +25,56 @@ class ElastiknnPlugin(settings: Settings) extends Plugin with IngestPlugin with 
       put(sparseBoolVector.CONTENT_TYPE, new sparseBoolVector.TypeParser)
       put(denseFloatVector.CONTENT_TYPE, new denseFloatVector.TypeParser)
     }
+  }
+
+  override def getSettings: util.List[Setting[_]] = util.List.of[Setting[_]](
+    ElastiknnPlugin.Settings.elastiknnFastDocValues
+  )
+
+  override def getEngineFactory(indexSettings: IndexSettings): Optional[EngineFactory] =
+    Optional.of {
+      new EngineFactory {
+        val codecService = new ElastiknnCodecService
+        override def newReadWriteEngine(config: EngineConfig): Engine = {
+          new InternalEngine(
+            new EngineConfig(
+              config.getShardId,
+              config.getAllocationId,
+              config.getThreadPool,
+              config.getIndexSettings,
+              config.getWarmer,
+              config.getStore,
+              config.getMergePolicy,
+              config.getAnalyzer,
+              config.getSimilarity,
+              codecService,
+              config.getEventListener,
+              config.getQueryCache,
+              config.getQueryCachingPolicy,
+              config.getTranslogConfig,
+              config.getFlushMergesAfter,
+              config.getExternalRefreshListener,
+              config.getInternalRefreshListener,
+              config.getIndexSort,
+              config.getCircuitBreakerService,
+              config.getGlobalCheckpointSupplier,
+              config.retentionLeasesSupplier,
+              config.getPrimaryTermSupplier,
+              config.getTombstoneDocSupplier
+            ))
+        }
+      }
+    }
+//    if (indexSettings.getValue(ElastiknnPlugin.Settings.elastiknnFastDocValues)) {
+//      ???
+//    } else Optional.empty[EngineFactory]
+}
+
+object ElastiknnPlugin {
+
+  object Settings {
+    val elastiknnFastDocValues: Setting[lang.Boolean] =
+      Setting.boolSetting("index.elastiknn.fast_doc_values", true, Setting.Property.IndexScope)
   }
 
 }
---
+++ elastiknn-plugin/src/main/scala/com/klibisz/elastiknn/codec/Elastiknn86Codec.scala
@@ -0,0 +1,26 @@
+package com.klibisz.elastiknn.codec
+
+import org.apache.lucene.codecs._
+import org.apache.lucene.codecs.lucene70.Lucene70DocValuesFormat
+
+class Elastiknn86Codec extends Codec(Elastiknn86Codec.ELASTIKNN_86) {
+
+  private val luceneCodec = Codec.forName(Elastiknn86Codec.LUCENE_86)
+
+  override def docValuesFormat(): DocValuesFormat = new Lucene70DocValuesFormat()
+
+  override def postingsFormat(): PostingsFormat = luceneCodec.postingsFormat()
+  override def storedFieldsFormat(): StoredFieldsFormat = luceneCodec.storedFieldsFormat()
+  override def termVectorsFormat(): TermVectorsFormat = luceneCodec.termVectorsFormat()
+  override def fieldInfosFormat(): FieldInfosFormat = luceneCodec.fieldInfosFormat()
+  override def segmentInfoFormat(): SegmentInfoFormat = luceneCodec.segmentInfoFormat()
+  override def normsFormat(): NormsFormat = luceneCodec.normsFormat()
+  override def liveDocsFormat(): LiveDocsFormat = luceneCodec.liveDocsFormat()
+  override def compoundFormat(): CompoundFormat = luceneCodec.compoundFormat()
+  override def pointsFormat(): PointsFormat = luceneCodec.pointsFormat()
+}
+
+object Elastiknn86Codec {
+  val ELASTIKNN_86 = "Elastiknn86Codec"
+  val LUCENE_86 = "Lucene86"
+}
