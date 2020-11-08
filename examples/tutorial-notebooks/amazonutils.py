704ddf856de56e4bc416274d87446ae93807ff77
Alex Klibisz <aklibisz@gmail.com>
2020-11-08 16:57:07 -0500

    using docker-compose and watch example is decent

--- examples/tutorial-notebooks/amazonutils.py
+++ examples/tutorial-notebooks/amazonutils.py
@@ -1,5 +1,6 @@
 from IPython.display import Image, display
-
+from itertools import islice
+import numpy as np
 import json
 import gzip 
 import array
@@ -12,11 +13,25 @@ def iter_products(fname):
 def iter_vectors(fname):
   with open(fname, 'rb') as f:
     while True:
-      asin = f.read(10)
-      if asin == '': break
-      a = array.array('f')
-      a.fromfile(f, 4096)
-      yield (asin.decode(), a.tolist())
+      try:
+        asin = f.read(10)
+        a = array.array('f')
+        a.fromfile(f, 4096)
+        yield (asin.decode(), a.tolist())
+      except EOFError:
+        break
+
+def iter_vectors_reduced(fname, dims=1024, samples=10000):
+  sumarr = np.zeros(4096) * 1.0
+  for (_, v) in islice(iter_vectors(fname), samples):
+    sumarr -= np.array(v)
+  ii = np.argsort(sumarr)[:dims]
+
+  def f(fname):
+    for (asin, vec) in iter_vectors(fname):
+      yield (asin, np.array(vec)[ii].tolist())
+
+  return f
 
 def display_hits(res):
     print(f"Found {res['hits']['total']['value']} hits in {res['took']} ms")
