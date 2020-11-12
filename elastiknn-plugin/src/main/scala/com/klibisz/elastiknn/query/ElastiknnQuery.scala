1755ec1e8e6a31aa6ca1c2128aa9dd971f155a12
Alex Klibisz <aklibisz@gmail.com>
2020-11-11 23:17:33 -0500

    NearestNeighborsQuerySpec passes

--- elastiknn-plugin/src/main/scala/com/klibisz/elastiknn/query/ElastiknnQuery.scala
+++ elastiknn-plugin/src/main/scala/com/klibisz/elastiknn/query/ElastiknnQuery.scala
@@ -4,21 +4,22 @@ import com.klibisz.elastiknn.ElastiknnException.ElastiknnRuntimeException
 import com.klibisz.elastiknn.api.NearestNeighborsQuery._
 import com.klibisz.elastiknn.api._
 import com.klibisz.elastiknn.mapper.VectorMapper
-import com.klibisz.elastiknn.models.Cache
+import com.klibisz.elastiknn.models.{Cache, SparseIndexedSimilarityFunction}
 import org.apache.lucene.index.IndexReader
 import org.apache.lucene.search.Query
 import org.elasticsearch.common.lucene.search.function.ScoreFunction
 import org.elasticsearch.index.mapper.MappedFieldType
 import org.elasticsearch.index.query.QueryShardContext
 
+import scala.language.implicitConversions
 import scala.util._
 
+/**
+  * Useful way to represent a query. The name is meh.
+  */
 trait ElastiknnQuery[V <: Vec] {
-
   def toLuceneQuery(indexReader: IndexReader): Query
-
   def toScoreFunction(indexReader: IndexReader): ScoreFunction
-
 }
 
 object ElastiknnQuery {
@@ -46,48 +47,55 @@ object ElastiknnQuery {
 
   def apply(query: NearestNeighborsQuery, mapping: Mapping): Try[ElastiknnQuery[_]] = {
     import com.klibisz.elastiknn.models.{ExactSimilarityFunction => ESF}
+    implicit def toSuccess[A <: Vec](q: ElastiknnQuery[A]): Try[ElastiknnQuery[A]] = Success(q)
     (query, mapping) match {
 
       case (Exact(f, Similarity.Jaccard, v: Vec.SparseBool),
             _: Mapping.SparseBool | _: Mapping.SparseIndexed | _: Mapping.JaccardLsh | _: Mapping.HammingLsh) =>
-        Success(new ExactQuery(f, v, ESF.Jaccard))
+        new ExactQuery(f, v, ESF.Jaccard)
 
       case (Exact(f, Similarity.Hamming, v: Vec.SparseBool),
             _: Mapping.SparseBool | _: Mapping.SparseIndexed | _: Mapping.JaccardLsh | _: Mapping.HammingLsh) =>
-        Success(new ExactQuery(f, v, ESF.Hamming))
+        new ExactQuery(f, v, ESF.Hamming)
 
       case (Exact(f, Similarity.L1, v: Vec.DenseFloat),
             _: Mapping.DenseFloat | _: Mapping.AngularLsh | _: Mapping.L2Lsh | _: Mapping.PermutationLsh) =>
-        Success(new ExactQuery(f, v, ESF.L1))
+        new ExactQuery(f, v, ESF.L1)
 
       case (Exact(f, Similarity.L2, v: Vec.DenseFloat),
             _: Mapping.DenseFloat | _: Mapping.AngularLsh | _: Mapping.L2Lsh | _: Mapping.PermutationLsh) =>
-        Success(new ExactQuery(f, v, ESF.L2))
+        new ExactQuery(f, v, ESF.L2)
 
       case (Exact(f, Similarity.Angular, v: Vec.DenseFloat),
             _: Mapping.DenseFloat | _: Mapping.AngularLsh | _: Mapping.L2Lsh | _: Mapping.PermutationLsh) =>
-        Success(new ExactQuery(f, v, ESF.Angular))
+        new ExactQuery(f, v, ESF.Angular)
+
+      case (SparseIndexed(f, Similarity.Jaccard, sbv: Vec.SparseBool), _: Mapping.SparseIndexed) =>
+        new SparseIndexedQuery(f, sbv, SparseIndexedSimilarityFunction.Jaccard)
+
+      case (SparseIndexed(f, Similarity.Hamming, sbv: Vec.SparseBool), _: Mapping.SparseIndexed) =>
+        new SparseIndexedQuery(f, sbv, SparseIndexedSimilarityFunction.Hamming)
 
       case (JaccardLsh(f, candidates, v: Vec.SparseBool, l: Float), m: Mapping.JaccardLsh) =>
-        Success(new HashingQuery(f, v, candidates, l, Cache(m).hash(v.trueIndices, v.totalIndices), ESF.Jaccard))
+        new HashingQuery(f, v, candidates, l, Cache(m).hash(v.trueIndices, v.totalIndices), ESF.Jaccard)
 
       case (HammingLsh(f, candidates, v: Vec.SparseBool, l: Float), m: Mapping.HammingLsh) =>
-        Success(new HashingQuery(f, v, candidates, l, Cache(m).hash(v.trueIndices, v.totalIndices), ESF.Hamming))
+        new HashingQuery(f, v, candidates, l, Cache(m).hash(v.trueIndices, v.totalIndices), ESF.Hamming)
 
       case (AngularLsh(f, candidates, v: Vec.DenseFloat, l: Float), m: Mapping.AngularLsh) =>
-        Success(new HashingQuery(f, v, candidates, l, Cache(m).hash(v.values), ESF.Angular))
+        new HashingQuery(f, v, candidates, l, Cache(m).hash(v.values), ESF.Angular)
 
       case (L2Lsh(f, candidates, probes, v: Vec.DenseFloat, l: Float), m: Mapping.L2Lsh) =>
-        Success(new HashingQuery(f, v, candidates, l, Cache(m).hash(v.values, probes), ESF.L2))
+        new HashingQuery(f, v, candidates, l, Cache(m).hash(v.values, probes), ESF.L2)
 
       case (PermutationLsh(f, Similarity.Angular, candidates, v: Vec.DenseFloat, l: Float), m: Mapping.PermutationLsh) =>
-        Success(new HashingQuery(f, v, candidates, l, Cache(m).hash(v.values), ESF.Angular))
+        new HashingQuery(f, v, candidates, l, Cache(m).hash(v.values), ESF.Angular)
 
       case (PermutationLsh(f, Similarity.L2, candidates, v: Vec.DenseFloat, l: Float), m: Mapping.PermutationLsh) =>
-        Success(new HashingQuery(f, v, candidates, l, Cache(m).hash(v.values), ESF.L2))
+        new HashingQuery(f, v, candidates, l, Cache(m).hash(v.values), ESF.L2)
 
       case (PermutationLsh(f, Similarity.L1, candidates, v: Vec.DenseFloat, l: Float), m: Mapping.PermutationLsh) =>
-        Success(new HashingQuery(f, v, candidates, l, Cache(m).hash(v.values), ESF.L1))
+        new HashingQuery(f, v, candidates, l, Cache(m).hash(v.values), ESF.L1)
 
       case _ => Failure(incompatible(query, mapping))
     }
