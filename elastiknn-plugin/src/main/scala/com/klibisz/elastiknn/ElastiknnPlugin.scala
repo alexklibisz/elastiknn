93c1bbf98aa2fa1f087096b77a62a18019e284b2
Alex Klibisz <aklibisz@gmail.com>
2021-03-27 22:22:38 -0400

    Update cryptic ES mode string in ClusterSpec

--- elastiknn-plugin/src/main/scala/com/klibisz/elastiknn/ElastiknnPlugin.scala
+++ elastiknn-plugin/src/main/scala/com/klibisz/elastiknn/ElastiknnPlugin.scala
@@ -1,23 +1,23 @@
 package com.klibisz.elastiknn
 
-import java.util
-import java.util.{Collections, Optional}
-
-import com.klibisz.elastiknn.codec.ElastiknnCodecService
 import com.klibisz.elastiknn.mapper.VectorMapper
 import com.klibisz.elastiknn.query._
 import org.elasticsearch.common.settings.{Setting, Settings}
 import org.elasticsearch.index.IndexSettings
-import org.elasticsearch.index.engine.{Engine, EngineConfig, EngineFactory, InternalEngine}
+import org.elasticsearch.index.engine.EngineFactory
 import org.elasticsearch.index.mapper.Mapper
 import org.elasticsearch.plugins.SearchPlugin.{QuerySpec, ScoreFunctionSpec}
 import org.elasticsearch.plugins._
 
+import java.util
+import java.util.{Collections, Optional}
+
 class ElastiknnPlugin(settings: Settings) extends Plugin with SearchPlugin with MapperPlugin with EnginePlugin {
 
-  override def getQueries: util.List[SearchPlugin.QuerySpec[_]] = Collections.singletonList(
-    new QuerySpec(KnnQueryBuilder.NAME, KnnQueryBuilder.Reader, KnnQueryBuilder.Parser)
-  )
+  override def getQueries: util.List[SearchPlugin.QuerySpec[_]] =
+    Collections.singletonList(
+      new QuerySpec(KnnQueryBuilder.NAME, KnnQueryBuilder.Reader, KnnQueryBuilder.Parser)
+    )
 
   override def getMappers: util.Map[String, Mapper.TypeParser] = {
     import VectorMapper._
@@ -35,38 +35,9 @@ class ElastiknnPlugin(settings: Settings) extends Plugin with SearchPlugin with 
     )
 
   override def getEngineFactory(indexSettings: IndexSettings): Optional[EngineFactory] = {
-    if (indexSettings.getValue(ElastiknnPlugin.Settings.elastiknn)) Optional.of {
-      new EngineFactory {
-        val codecService = new ElastiknnCodecService
-        override def newReadWriteEngine(config: EngineConfig): Engine = {
-          new InternalEngine(
-            new EngineConfig(
-              config.getShardId,
-              config.getThreadPool,
-              config.getIndexSettings,
-              config.getWarmer,
-              config.getStore,
-              config.getMergePolicy,
-              config.getAnalyzer,
-              config.getSimilarity,
-              codecService,
-              config.getEventListener,
-              config.getQueryCache,
-              config.getQueryCachingPolicy,
-              config.getTranslogConfig,
-              config.getFlushMergesAfter,
-              config.getExternalRefreshListener,
-              config.getInternalRefreshListener,
-              config.getIndexSort,
-              config.getCircuitBreakerService,
-              config.getGlobalCheckpointSupplier,
-              config.retentionLeasesSupplier,
-              config.getPrimaryTermSupplier,
-              config.getTombstoneDocSupplier,
-              config.getSnapshotCommitSupplier
-            ))
-        }
-      }
+    if (indexSettings.getValue(ElastiknnPlugin.Settings.elastiknn)) {
+      // TODO: log that this setting is deprecated.
+      Optional.empty()
     } else Optional.empty()
   }
 }
--- elastiknn-plugin/src/main/scala/com/klibisz/elastiknn/codec/ElastiknnCodecService.scala
+++ elastiknn-plugin/src/main/scala/com/klibisz/elastiknn/codec/ElastiknnCodecService.scala
@@ -14,4 +14,5 @@ object ElastiknnCodecService {
   val ELASTIKNN_84 = "Elastiknn84Codec"
   val ELASTIKNN_86 = "Elastiknn86Codec"
   val ELASTIKNN_87 = "Elastiknn87Codec"
+  val ELASTIKNN_88 = "Elastiknn88Codec"
 }
