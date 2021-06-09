3cd7d35a61c95c80411b5df5795fa80cc4508d48
Alex Klibisz <aklibisz@gmail.com>
2021-06-08 22:44:13 -0400

    Some progress but some tests still fail

--- elastiknn-client-elastic4s/src/main/scala/com/klibisz/elastiknn/client/Elastic4sCompatibility.scala
+++ elastiknn-client-elastic4s/src/main/scala/com/klibisz/elastiknn/client/Elastic4sCompatibility.scala
@@ -1,22 +1,17 @@
 package com.klibisz.elastiknn.client
 
-import com.klibisz.elastiknn.ELASTIKNN_NAME
 import com.klibisz.elastiknn.api.{ElasticsearchCodec, NearestNeighborsQuery}
-import com.sksamuel.elastic4s.json.{XContentBuilder, XContentFactory}
-import com.sksamuel.elastic4s.requests.searches.queries.{CustomQuery, Query}
+import com.sksamuel.elastic4s.requests.searches.queries.{Query, RawQuery}
 
 import scala.language.implicitConversions
 
-object Elastic4sCompatibility {
+trait Elastic4sCompatibility {
 
   implicit def convertQuery(nnq: NearestNeighborsQuery): Query = nnq.toQuery
 
   implicit class NearestNeighborsQueryCompat(nnq: NearestNeighborsQuery) {
-    def toQuery: Query = new CustomQuery {
-      override def buildQueryBody(): XContentBuilder =
-        XContentFactory.jsonBuilder.rawField(s"${ELASTIKNN_NAME}_nearest_neighbors", ElasticsearchCodec.nospaces(nnq))
-
-    }
+    def toQuery: Query = RawQuery(s"""{"elastiknn_nearest_neighbors":${ElasticsearchCodec.nospaces(nnq)}}""")
   }
-
 }
+
+object Elastic4sCompatibility extends Elastic4sCompatibility
