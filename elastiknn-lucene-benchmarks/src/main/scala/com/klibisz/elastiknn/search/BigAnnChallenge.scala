c08348e89a25e742381d6a4d0e7199927c07f6a9
Alex Klibisz <aklibisz@gmail.com>
2021-06-27 15:31:38 -0400

    Parallelized the dataset streaming

--- elastiknn-lucene-benchmarks/src/main/scala/com/klibisz/elastiknn/search/BigAnnChallenge.scala
+++ elastiknn-lucene-benchmarks/src/main/scala/com/klibisz/elastiknn/search/BigAnnChallenge.scala
@@ -4,7 +4,7 @@ import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream.alpakka.lucene.scaladsl.LuceneSink
 import akka.stream.scaladsl._
-import akka.stream.{ActorMaterializer, IOResult, Materializer}
+import akka.stream.{ActorMaterializer, IOResult}
 import com.klibisz.elastiknn.models.{HashingModel, L2LshModel}
 import org.apache.lucene.document.{Document, Field, FieldType}
 import org.apache.lucene.index.{IndexOptions, IndexWriterConfig}
@@ -16,14 +16,22 @@ import java.util.Random
 import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, Future}
 
-sealed trait Datatype
+sealed trait Datatype {
+  def chunkSize: Int
+}
 object Datatype {
-  case object U8Bin extends Datatype
-  case object FBin extends Datatype
-  case object I8Bin extends Datatype
+  case object U8Bin extends Datatype {
+    override def chunkSize: Int = 1
+  }
+  case object I8Bin extends Datatype {
+    override def chunkSize: Int = 1
+  }
+  case object FBin extends Datatype {
+    override def chunkSize: Int = 4
+  }
 }
 
-case class Dataset(path: Path, dims: Int, dtype: Datatype)
+final case class Dataset(path: Path, dims: Int, dtype: Datatype)
 
 object Dataset {
   val sift1b = Dataset(Paths.get("/home/alex/Desktop/big-ann-benchmarks/bigann/learn.100M.u8bin"), 128, Datatype.U8Bin)
@@ -33,44 +41,59 @@ object Dataset {
 
 case class VecDoc(docId: String, values: Array[Float])
 
-object Streaming {
+object Utils {
 
   /**
     * Read a dataset from disk.
-    * @return Source of (vector, ID) tuples.
     */
-  def streamDataset(dataset: Dataset)(implicit mat: Materializer): Source[VecDoc, Future[IOResult]] = {
+  def streamDataset(dataset: Dataset, parallelism: Int)(implicit ec: ExecutionContext): Source[VecDoc, Future[IOResult]] = {
     dataset.dtype match {
       case Datatype.U8Bin =>
         FileIO
-          .fromPath(dataset.path, chunkSize = dataset.dims, startPosition = 8)
+          .fromPath(dataset.path, chunkSize = dataset.dims * dataset.dtype.chunkSize, startPosition = 8)
           .zipWithIndex
-          .map {
-            case (bs, ix) => VecDoc(s"v$ix", bs.map(b => (b.toInt & 0xFF).toFloat).toArray)
+          .mapAsyncUnordered(parallelism) {
+            case (bs, ix) =>
+              Future {
+                val arr = new Array[Byte](dataset.dims)
+                bs.asByteBuffer.get(arr, 0, arr.length)
+                VecDoc(s"v$ix", arr.map(b => (b.toInt & 0xFF).toFloat))
+              }
           }
       case Datatype.I8Bin =>
         FileIO
-          .fromPath(dataset.path, chunkSize = dataset.dims, startPosition = 8)
+          .fromPath(dataset.path, chunkSize = dataset.dims * dataset.dtype.chunkSize, startPosition = 8)
           .zipWithIndex
-          .map {
-            case (bs, ix) => VecDoc(s"v$ix", bs.map(_.toInt.toFloat).toArray)
+          .mapAsyncUnordered(parallelism) {
+            case (bs, ix) =>
+              Future {
+                val arr = new Array[Byte](dataset.dims)
+                bs.asByteBuffer.get(arr, 0, arr.length)
+                VecDoc(s"v$ix", arr.map(_.toFloat))
+              }
           }
       case _ =>
         FileIO
-          .fromPath(dataset.path, chunkSize = dataset.dims * 4, startPosition = 8)
+          .fromPath(dataset.path, chunkSize = dataset.dims * dataset.dtype.chunkSize, startPosition = 8)
           .zipWithIndex
-          .map {
+          .mapAsyncUnordered(parallelism) {
             case (bs, ix) =>
-              VecDoc(s"v$ix", bs.grouped(4).toArray.map(_.asByteBuffer.order(ByteOrder.LITTLE_ENDIAN).getFloat))
+              Future {
+                val arr = new Array[Float](dataset.dims)
+                val buf = bs.asByteBuffer.alignedSlice(4).order(ByteOrder.LITTLE_ENDIAN).asFloatBuffer()
+                buf.get(arr, 0, arr.length)
+                VecDoc(s"v$ix", arr)
+              }
           }
     }
   }
 
   def indexWithHashingModel(
       model: HashingModel.DenseFloat,
+      parallelism: Int,
       vecFieldName: String = "vec",
       idFieldName: String = "id"
-  ): Flow[VecDoc, Document, NotUsed] = {
+  )(implicit ec: ExecutionContext): Flow[VecDoc, Document, NotUsed] = {
 
     val idFieldType = new FieldType()
     idFieldType.setStored(true)
@@ -82,12 +105,14 @@ object Streaming {
     vecFieldType.setStoreTermVectors(false)
 
     Flow[VecDoc]
-      .map {
+      .mapAsyncUnordered(parallelism) {
         case VecDoc(id, vec) =>
-          val d = new Document()
-          d.add(new Field(idFieldName, id, idFieldType))
-          model.hash(vec).foreach(hf => d.add(new Field(vecFieldName, hf.hash, vecFieldType)))
-          d
+          Future {
+            val d = new Document()
+            d.add(new Field(idFieldName, id, idFieldType))
+            model.hash(vec).foreach(hf => d.add(new Field(vecFieldName, hf.hash, vecFieldType)))
+            d
+          }
       }
   }
 }
@@ -98,17 +123,19 @@ object BigAnnChallenge extends App {
   implicit val system = ActorSystem()
   implicit val materializer = ActorMaterializer
 
-  val dataset = Dataset.yandex1b
+  val parallelism = 8
+  val dataset = Dataset.space1b
+
   val model = new L2LshModel(dataset.dims, 40, 3, 5, new Random(0))
+
   val tmpDir = Files.createTempDirectory("elastiknn-lsh-")
   val indexDirectory = new MMapDirectory(tmpDir)
   val indexConfig = new IndexWriterConfig().setMaxBufferedDocs(100000)
 
-  println(tmpDir)
-
-  val run = Streaming
-    .streamDataset(dataset)
-    .via(Streaming.indexWithHashingModel(model))
+  val run = Utils
+    .streamDataset(dataset, parallelism)
+    //    .take(1000000)
+    .via(Utils.indexWithHashingModel(model, parallelism))
     .runWith(LuceneSink.create(indexDirectory, indexConfig))
 
   try Await.result(run, Duration.Inf)
