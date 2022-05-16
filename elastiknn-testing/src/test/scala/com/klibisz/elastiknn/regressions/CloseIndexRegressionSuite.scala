7433df7c756b610a73490deb1d6e10e8acae3eb6
Alex Klibisz <aklibisz@gmail.com>
2022-05-15 22:53:06 -0400

    Fixed CloseIndexRegressionSuite

--- elastiknn-testing/src/test/scala/com/klibisz/elastiknn/regressions/CloseIndexRegressionSuite.scala
+++ elastiknn-testing/src/test/scala/com/klibisz/elastiknn/regressions/CloseIndexRegressionSuite.scala
@@ -3,8 +3,6 @@ package com.klibisz.elastiknn.regressions
 import com.klibisz.elastiknn.api.Vec
 import com.klibisz.elastiknn.testing.ElasticAsyncClient
 import com.sksamuel.elastic4s.ElasticDsl._
-import com.sksamuel.elastic4s.requests.indexes.admin.CloseIndexResponse
-import com.sksamuel.elastic4s.{ElasticRequest, Handler}
 import org.scalatest.Assertions
 import org.scalatest.funsuite.AsyncFunSuite
 import org.scalatest.matchers.should.Matchers
@@ -14,13 +12,6 @@ import scala.util.Random
 class CloseIndexRegressionSuite extends AsyncFunSuite with Matchers with ElasticAsyncClient {
 
   implicit val rng = new Random(0)
-
-  case class FreezeRequest(index: String)
-  implicit object FreezeHandler extends Handler[FreezeRequest, CloseIndexResponse] {
-    override def build(t: FreezeRequest): ElasticRequest = ElasticRequest("POST", s"/${t.index}/_freeze")
-  }
-
-  def freezeIndex(index: String): FreezeRequest = FreezeRequest(index)
 
   test("close index without elastiknn setting") {
     val index = "issue-215-close-no-elastiknn"
@@ -49,33 +40,4 @@ class CloseIndexRegressionSuite extends AsyncFunSuite with Matchers with Elastic
       _ <- deleteIfExists(index)
     } yield Assertions.succeed
   }
-
-  test("freeze index without elastiknn setting") {
-    val index = "issue-215-freeze-no-elastiknn"
-    val corpus = Vec.DenseFloat.randoms(42, 10000)
-    for {
-      _ <- deleteIfExists(index)
-      _ <- eknn.execute(createIndex(index).shards(1).indexSetting("elastiknn", false))
-      _ <- eknn.index(index, "vec", corpus, "id", corpus.indices.map(_ + 1).map(_.toString))
-      _ <- eknn.execute(refreshIndex(index))
-      _ <- eknn.execute(forceMerge(index).maxSegments(1))
-      _ <- eknn.execute(freezeIndex(index))
-      _ <- deleteIfExists(index)
-    } yield Assertions.succeed
-  }
-
-  ignore("freeze index with elastiknn setting") {
-    val index = "issue-215-freeze-elastiknn"
-    val corpus = Vec.DenseFloat.randoms(42, 10000)
-    for {
-      _ <- deleteIfExists(index)
-      _ <- eknn.execute(createIndex(index).shards(1).indexSetting("elastiknn", true))
-      _ <- eknn.index(index, "vec", corpus, "id", corpus.indices.map(_ + 1).map(_.toString))
-      _ <- eknn.execute(refreshIndex(index))
-      _ <- eknn.execute(forceMerge(index).maxSegments(1))
-      _ <- eknn.execute(freezeIndex(index))
-      _ <- deleteIfExists(index)
-    } yield Assertions.succeed
-  }
-
 }
