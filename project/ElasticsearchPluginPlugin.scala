32ea439c494bfbba9b9bdcab3811afa91daa5f91
Alex Klibisz <aklibisz@gmail.com>
2022-09-21 08:13:57 -0500

    WIP: progress

--- project/ElasticsearchPluginPlugin.scala
+++ project/ElasticsearchPluginPlugin.scala
@@ -1,4 +1,5 @@
 import sbt.Keys._
+import sbt.internal.graph.{GraphModuleId, ModuleGraph}
 import sbt.internal.graph.backend.SbtUpdateReport
 import sbt.{Def, _}
 
@@ -13,38 +14,40 @@ object ElasticsearchPluginPlugin extends AutoPlugin {
   import autoImport._
 
   override lazy val projectSettings: Seq[Def.Setting[_]] = List(
-    libraryDependencies ++= Seq(
-      "org.elasticsearch" % "elasticsearch" % elasticsearchVersion.value
-    ),
+    libraryDependencies ++= Seq(elasticsearchModule(elasticsearchVersion.value)),
     bundlePlugin := bundlePluginTask.value
   )
+
+  private def elasticsearchModule(version: String): ModuleID = "org.elasticsearch" % "elasticsearch" % version
 
   private def bundlePluginTask: Def.Initialize[Task[Unit]] = Def.task {
     val log = sLog.value
 
-    // Figure out how to get rid of elasticsearch and its transitive dependencies.
+    // We have to exclude Elasticsearch and any of its transitive dependencies in order to keep the zip file small.
+    // To do this, traverse the ModuleGraph and build a list of any module that depends on the Elasticsearch module.
+    // TODO: Figure out how to also ignore transitive dependencies of Elasticsearch.
+    val esMod = elasticsearchModule(elasticsearchVersion.value)
+    val ignoredModules: Seq[ModuleID] = esMod +: (for {
+      cr <- Classpaths.updateTask.value.configurations
+      if cr.configuration.name == Configurations.Compile.name
+      mg = SbtUpdateReport.fromConfigurationReport(cr, projectID.value)
+      (from, to) <- mg.edges
+      if from.organization == esMod.organization && from.name == esMod.name && from.version == esMod.revision
+    } yield to.organization % to.name % to.version)
 
-    val ignoredModules = for {
-      c <- Classpaths.updateTask.value.configurations
-      if c.configuration.name == "compile"
-    } yield {
-      println((c.configuration.name, c.modules.length, c.details.length))
-      val moduleGraph = SbtUpdateReport.fromConfigurationReport(c, projectID.value)
-      println(moduleGraph.nodes.length)
-      println(moduleGraph.edges.length)
-      moduleGraph.edges.foreach(println(_))
-    }
+    val jars = (Compile / dependencyClasspathAsJars)
+      .value
+      .filterNot {
+        af: Attributed[File] =>
+          val moduleIdOpt = af.metadata.entries.map(_.value).collectFirst {
+            case m: ModuleID => m.organization % m.name % m.revision
+          }
+          val moduleId = moduleIdOpt.getOrElse(throw new IllegalStateException(s"JAR ${af.data.getName} missing required ModuleID metadata"))
+          ignoredModules.contains(moduleId)
+      }
 
-//    // allDependencies
-//    val jars = (Compile / dependencyClasspathAsJars)
-//      .value
-//      .filterNot {
-//        af: Attributed[File] =>
-//          af.data.getPath.contains("org/elasticsearch/elasticsearch") ||
-//            af.data.getPath.contains("org/apache/lucene/lucene")
-//      }
-//    jars.sortBy(_.data.getName).foreach(j => println(j.data.getName))
-//    println(jars.length)
-    log.info(s"This is the bundlePlugin task: ${elasticsearchPluginVersion.value}, ${elasticsearchPluginDescription.value}, ${elasticsearchPluginName.value}")
+    jars.map(_.data.getName).sorted.foreach(println(_))
+
+//    log.info(s"This is the bundlePlugin task: ${elasticsearchPluginVersion.value}, ${elasticsearchPluginDescription.value}, ${elasticsearchPluginName.value}")
   }
 }
