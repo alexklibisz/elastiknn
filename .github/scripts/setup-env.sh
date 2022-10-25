7f4bd7e6a0145bab11f02dca58ff909d21e756fb
Alex Klibisz <aklibisz@gmail.com>
2022-10-25 08:18:28 -0500

    Build: Remove release steps for old site

--- .github/scripts/setup-env.sh
+++ .github/scripts/setup-env.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -e
 
-# SSH
-mkdir -p $HOME/.ssh
-echo $SSH_CONFIG_B64 | base64 --decode > $HOME/.ssh/config
-echo $SSH_IDRSA_B64 | base64 --decode > $HOME/.ssh/elastiknn-site
-chmod 400 $HOME/.ssh/elastiknn-site
-
 # PYPI
 echo $PYPIRC_B64 | base64 --decode > $HOME/.pypirc
