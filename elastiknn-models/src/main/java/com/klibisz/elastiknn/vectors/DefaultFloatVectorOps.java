b1f24373e7bdb612d2e3d1b04a8d01fc5990ee68
Alex Klibisz <aklibisz@gmail.com>
2023-02-27 07:48:11 -0800

    Some internal cleanup from previous PR

--- elastiknn-models/src/main/java/com/klibisz/elastiknn/vectors/DefaultFloatVectorOps.java
+++ elastiknn-models/src/main/java/com/klibisz/elastiknn/vectors/DefaultFloatVectorOps.java
@@ -19,25 +19,21 @@ public final class DefaultFloatVectorOps implements FloatVectorOps {
         else return -1;
     }
 
-    public double l1Distance(float[] v1, float[] v2) {
-        double sumAbsDiff = 0.0;
-        for (int i = 0; i < v1.length; i++) {
-            sumAbsDiff += Math.abs(v1[i] - v2[i]);
-        }
-        return sumAbsDiff;
+    public double dotProduct(float[] v1, float[] v2) {
+        float dotProd = 0f;
+        for (int i = 0; i < v1.length; i++) dotProd += v1[i] * v2[i];
+        return dotProd;
     }
 
-    public double dotProduct(float[] v1, float[] v2) {
-        float dp = 0f;
-        for (int i = 0; i < v1.length; i++) dp += v1[i] * v2[i];
-        return dp;
+    public double l1Distance(float[] v1, float[] v2) {
+        double sumAbsDiff = 0.0;
+        for (int i = 0; i < v1.length; i++) sumAbsDiff += Math.abs(v1[i] - v2[i]);
+        return sumAbsDiff;
     }
 
     public double l2Distance(float[] v1, float[] v2) {
         double sumSqrDiff = 0.0;
-        for (int i = 0; i < v1.length; i++) {
-            sumSqrDiff += Math.pow(v1[i] - v2[i], 2);
-        }
+        for (int i = 0; i < v1.length; i++) sumSqrDiff += Math.pow(v1[i] - v2[i], 2);
         return Math.sqrt(sumSqrDiff);
     }
 }
