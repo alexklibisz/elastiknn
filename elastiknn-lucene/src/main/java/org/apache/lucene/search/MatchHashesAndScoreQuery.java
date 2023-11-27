0cd95a468942132c980ea57d934ce5d748b32b71
Alex Klibisz <8015228+alexklibisz@users.noreply.github.com>
2023-11-26 22:02:40 -0800

    Remove counterLimit checks and put back Math.min(docs.freq(), hf.freq())

--- elastiknn-lucene/src/main/java/org/apache/lucene/search/MatchHashesAndScoreQuery.java
+++ elastiknn-lucene/src/main/java/org/apache/lucene/search/MatchHashesAndScoreQuery.java
@@ -63,14 +63,13 @@ public class MatchHashesAndScoreQuery extends Query {
                     TermsEnum termsEnum = terms.iterator();
                     PostingsEnum docs = null;
                     HitCounter counter = new ArrayHitCounter(reader.maxDoc());
-                    double counterLimit = counter.capacity() + 1;
                     // TODO: Is this the right place to use the live docs bitset to check for deleted docs?
                     // Bits liveDocs = reader.getLiveDocs();
                     for (HashAndFreq hf : hashAndFrequencies) {
-                        if (counter.numHits() < counterLimit && termsEnum.seekExact(new BytesRef(hf.hash))) {
+                        if (termsEnum.seekExact(new BytesRef(hf.hash))) {
                             docs = termsEnum.postings(docs, PostingsEnum.NONE);
-                            while (docs.nextDoc() != DocIdSetIterator.NO_MORE_DOCS && counter.numHits() < counterLimit) {
-                                counter.increment(docs.docID(), 1);
+                            while (docs.nextDoc() != DocIdSetIterator.NO_MORE_DOCS) {
+                                counter.increment(docs.docID(), Math.min(docs.freq(), hf.freq));
                             }
                         }
                     }
