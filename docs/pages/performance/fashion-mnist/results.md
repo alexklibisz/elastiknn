ac3fed8544f896aef609356f8c29e1ec706fe383
Alex Klibisz <8015228+alexklibisz@users.noreply.github.com>
2023-12-03 13:55:54 -0500

    Performance: use parallel accumulators to speed up PanamaFloatVectorOps dotProduct and l2Distance (96% recall at 204 qps) (#620)

--- docs/pages/performance/fashion-mnist/results.md
+++ docs/pages/performance/fashion-mnist/results.md
@@ -1,10 +1,10 @@
 |Model|Parameters|Recall|Queries per Second|
 |---|---|---|---|
-|eknn-l2lsh|L=100 k=4 w=1024 candidates=500 probes=0|0.378|349.851|
-|eknn-l2lsh|L=100 k=4 w=1024 candidates=1000 probes=0|0.446|296.219|
-|eknn-l2lsh|L=100 k=4 w=1024 candidates=500 probes=3|0.635|286.468|
-|eknn-l2lsh|L=100 k=4 w=1024 candidates=1000 probes=3|0.716|244.536|
-|eknn-l2lsh|L=100 k=4 w=2048 candidates=500 probes=0|0.767|315.023|
-|eknn-l2lsh|L=100 k=4 w=2048 candidates=1000 probes=0|0.847|264.479|
-|eknn-l2lsh|L=100 k=4 w=2048 candidates=500 probes=3|0.922|220.714|
-|eknn-l2lsh|L=100 k=4 w=2048 candidates=1000 probes=3|0.960|193.597|
+|eknn-l2lsh|L=100 k=4 w=1024 candidates=500 probes=0|0.378|377.213|
+|eknn-l2lsh|L=100 k=4 w=1024 candidates=1000 probes=0|0.447|320.000|
+|eknn-l2lsh|L=100 k=4 w=1024 candidates=500 probes=3|0.635|302.384|
+|eknn-l2lsh|L=100 k=4 w=1024 candidates=1000 probes=3|0.717|259.201|
+|eknn-l2lsh|L=100 k=4 w=2048 candidates=500 probes=0|0.767|335.978|
+|eknn-l2lsh|L=100 k=4 w=2048 candidates=1000 probes=0|0.846|287.923|
+|eknn-l2lsh|L=100 k=4 w=2048 candidates=500 probes=3|0.921|229.917|
+|eknn-l2lsh|L=100 k=4 w=2048 candidates=1000 probes=3|0.960|204.375|
--- elastiknn-models/src/main/java/com/klibisz/elastiknn/vectors/PanamaFloatVectorOps.java
+++ elastiknn-models/src/main/java/com/klibisz/elastiknn/vectors/PanamaFloatVectorOps.java
@@ -8,7 +8,21 @@ import java.util.Arrays;
 
 public final class PanamaFloatVectorOps implements FloatVectorOps {
 
-    final VectorSpecies<Float> species = FloatVector.SPECIES_PREFERRED;
+    private static final VectorSpecies<Float> species;
+    private static final int speciesLength;
+    private static final int speciesLengthTimes2;
+    private static final int speciesLengthTimes3;
+    private static final int speciesLengthTimes4;
+
+    static {
+        // Setting it in this static block seems to make a big
+        // improvement in performance.
+        species = FloatVector.SPECIES_PREFERRED;
+        speciesLength = species.length();
+        speciesLengthTimes2 = speciesLength * 2;
+        speciesLengthTimes3 = speciesLength * 3;
+        speciesLengthTimes4 = speciesLength * 4;
+    }
 
     public double cosineSimilarity(float[] v1, float[] v2) {
         double dotProd = 0.0;
@@ -36,6 +50,51 @@ public final class PanamaFloatVectorOps implements FloatVectorOps {
     }
 
     public double dotProduct(float[] v1, float[] v2) {
+        int i = 0;
+        double dotProd = 0d;
+        if (v1.length > speciesLengthTimes2) {
+            int bound;
+            // Maintain four accumulators to maximize parallelism.
+            // This unrolling trick makes a significant difference on benchmarks.
+            // Inspired by Lucene's PanamaVectorUtilSupport implementation.
+            // See https://github.com/alexklibisz/elastiknn/pull/620 for details.
+            FloatVector acc1 = FloatVector.zero(species);
+            FloatVector acc2 = FloatVector.zero(species);
+            FloatVector acc3 = FloatVector.zero(species);
+            FloatVector acc4 = FloatVector.zero(species);
+            FloatVector fv1, fv2;
+            for (bound = species.loopBound(v1.length - 3 * speciesLength); i < bound; i += speciesLengthTimes4) {
+                fv1 = FloatVector.fromArray(species, v1, i);
+                fv2 = FloatVector.fromArray(species, v2, i);
+                acc1 = acc1.add(fv1.mul(fv2));
+                FloatVector fv3 = FloatVector.fromArray(species, v1, i + speciesLength);
+                FloatVector fv4 = FloatVector.fromArray(species, v2, i + speciesLength);
+                acc2 = acc2.add(fv3.mul(fv4));
+                FloatVector fv5 = FloatVector.fromArray(species, v1, i + speciesLengthTimes2);
+                FloatVector fv6 = FloatVector.fromArray(species, v2, i + speciesLengthTimes2);
+                acc3 = acc3.add(fv5.mul(fv6));
+                FloatVector fv7 = FloatVector.fromArray(species, v1, i + speciesLengthTimes3);
+                FloatVector fv8 = FloatVector.fromArray(species, v2, i + speciesLengthTimes3);
+                acc4 = acc4.add(fv7.mul(fv8));
+            }
+            for (bound = species.loopBound(v1.length); i < bound; i += speciesLength) {
+                fv1 = FloatVector.fromArray(species, v1, i);
+                fv2 = FloatVector.fromArray(species, v2, i);
+                acc1 = acc1.add(fv1.mul(fv2));
+            }
+            fv1 = acc1.add(acc2);
+            fv2 = acc3.add(acc4);
+            dotProd += fv1.add(fv2).reduceLanes(VectorOperators.ADD);
+        }
+        while (i < v1.length) {
+            dotProd += v1[i] * v2[i];
+            ++i;
+        }
+        return dotProd;
+    }
+
+    // A simpler dot product implementation, kept around for benchmarking purposes.
+    public double dotProductSimple(float[] v1, float[] v2) {
         double dotProd = 0f;
         int i = 0;
         int bound = species.loopBound(v1.length);
@@ -50,7 +109,6 @@ public final class PanamaFloatVectorOps implements FloatVectorOps {
         }
         return dotProd;
     }
-
 
     public double l1Distance(float[] v1, float[] v2) {
         double sumAbsDiff = 0.0;
@@ -69,6 +127,57 @@ public final class PanamaFloatVectorOps implements FloatVectorOps {
     }
 
     public double l2Distance(float[] v1, float[] v2) {
+        int i = 0;
+        float sumSqrDiff = 0.0F;
+        if (v1.length > 2 * speciesLength) {
+            int bound;
+            // Maintain four accumulators to maximize parallelism.
+            // This unrolling trick makes a significant difference on benchmarks.
+            // Inspired by Lucene's PanamaVectorUtilSupport implementation.
+            // See https://github.com/alexklibisz/elastiknn/pull/620 for details.
+            FloatVector acc1 = FloatVector.zero(species);
+            FloatVector acc2 = FloatVector.zero(species);
+            FloatVector acc3 = FloatVector.zero(species);
+            FloatVector acc4 = FloatVector.zero(species);
+            FloatVector fv1, fv2, diff;
+            for(bound = species.loopBound(v1.length - 3 * species.length()); i < bound; i += speciesLengthTimes4) {
+                fv1 = FloatVector.fromArray(species, v1, i);
+                fv2 = FloatVector.fromArray(species, v2, i);
+                diff = fv1.sub(fv2);
+                acc1 = acc1.add(diff.mul(diff));
+                FloatVector fv3 = FloatVector.fromArray(species, v1, i + speciesLength);
+                FloatVector fv4 = FloatVector.fromArray(species, v2, i + speciesLength);
+                FloatVector diff2 = fv3.sub(fv4);
+                acc2 = acc2.add(diff2.mul(diff2));
+                FloatVector fv5 = FloatVector.fromArray(species, v1, i + speciesLengthTimes2);
+                FloatVector fv6 = FloatVector.fromArray(species, v2, i + speciesLengthTimes2);
+                FloatVector diff3 = fv5.sub(fv6);
+                acc3 = acc3.add(diff3.mul(diff3));
+                FloatVector fv7 = FloatVector.fromArray(species, v1, i + speciesLengthTimes3);
+                FloatVector fv8 = FloatVector.fromArray(species, v2, i + speciesLengthTimes3);
+                FloatVector diff4 = fv7.sub(fv8);
+                acc4 = acc4.add(diff4.mul(diff4));
+            }
+            for(bound = species.loopBound(v1.length); i < bound; i += species.length()) {
+                fv1 = FloatVector.fromArray(species, v1, i);
+                fv2 = FloatVector.fromArray(species, v2, i);
+                diff = fv1.sub(fv2);
+                acc1 = acc1.add(diff.mul(diff));
+            }
+            fv1 = acc1.add(acc2);
+            fv2 = acc3.add(acc4);
+            sumSqrDiff += fv1.add(fv2).reduceLanes(VectorOperators.ADD);
+        }
+        while(i < v1.length) {
+            float diff = v1[i] - v2[i];
+            sumSqrDiff += diff * diff;
+            ++i;
+        }
+        return Math.sqrt(sumSqrDiff);
+    }
+
+    // A simpler dot product implementation, kept around for benchmarking purposes.
+    public double l2DistanceSimple(float[] v1, float[] v2) {
         double sumSqrDiff = 0f;
         int i = 0;
         int bound = species.loopBound(v1.length);
