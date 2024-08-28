2ec50cef2258815715fa15767bb1d412c0fa5f2e
Alex Klibisz <aklibisz@gmail.com>
2024-08-28 16:18:09 -0700

    Fix compiler warnings

--- elastiknn-jmh-benchmarks/src/main/scala/com/klibisz/elastiknn/jmhbenchmarks/HitCounterBenchmarks.scala
+++ elastiknn-jmh-benchmarks/src/main/scala/com/klibisz/elastiknn/jmhbenchmarks/HitCounterBenchmarks.scala
@@ -2,9 +2,7 @@ package com.klibisz.elastiknn.jmhbenchmarks
 
 import com.klibisz.elastiknn.search.ArrayHitCounter
 import org.openjdk.jmh.annotations.*
-import org.apache.lucene.internal.hppc.IntIntHashMap
 import org.apache.lucene.search.DocIdSetIterator
-import org.eclipse.collections.impl.map.mutable.primitive.IntShortHashMap
 
 import scala.util.Random
 
@@ -21,7 +19,9 @@ class HitCounterBenchmarksFixtures {
 class HitCounterBenchmarks {
 
   private def consumeDocIdSetIterator(disi: DocIdSetIterator): Unit = {
-    while (disi.nextDoc() != DocIdSetIterator.NO_MORE_DOCS) disi.docID()
+    while (disi.nextDoc() != DocIdSetIterator.NO_MORE_DOCS) {
+      val _ = disi.docID()
+    }
     ()
   }
 
@@ -36,48 +36,4 @@ class HitCounterBenchmarks {
     consumeDocIdSetIterator(ahc.docIdSetIterator(f.candidates))
     ()
   }
-
-//  @Benchmark
-//  @BenchmarkMode(Array(Mode.Throughput))
-//  @Fork(value = 1)
-//  @Warmup(time = 5, iterations = 5)
-//  @Measurement(time = 5, iterations = 5)
-//  def arrayCountBaseline(f: HitCounterBenchmarksFixtures): Unit = {
-//    val arr = new Array[Int](f.numDocs)
-//    for (d <- f.docs) arr.update(d, arr(d) + 1)
-//    ()
-//  }
-//
-//  @Benchmark
-//  @BenchmarkMode(Array(Mode.Throughput))
-//  @Fork(value = 1)
-//  @Warmup(time = 5, iterations = 5)
-//  @Measurement(time = 5, iterations = 5)
-//  def hashMapGetOrDefault(f: HitCounterBenchmarksFixtures): Unit = {
-//    val h = new java.util.HashMap[Int, Int](f.initialMapSize, 0.99f)
-//    for (d <- f.docs) h.put(d, h.getOrDefault(d, 0) + 1)
-//    ()
-//  }
-//
-//  @Benchmark
-//  @BenchmarkMode(Array(Mode.Throughput))
-//  @Fork(value = 1)
-//  @Warmup(time = 5, iterations = 5)
-//  @Measurement(time = 5, iterations = 5)
-//  def luceneIntIntHashMap(f: HitCounterBenchmarksFixtures): Unit = {
-//    val m = new IntIntHashMap(f.initialMapSize, 0.99d)
-//    for (d <- f.docs) m.putOrAdd(d, 1, 1)
-//    ()
-//  }
-//
-//  @Benchmark
-//  @BenchmarkMode(Array(Mode.Throughput))
-//  @Fork(value = 1)
-//  @Warmup(time = 5, iterations = 5)
-//  @Measurement(time = 5, iterations = 5)
-//  def eclipseIntShortHashMapAddToValue(f: HitCounterBenchmarksFixtures): Unit = {
-//    val m = new IntShortHashMap(f.initialMapSize)
-//    for (d <- f.docs) m.addToValue(d, 1)
-//    ()
-//  }
 }
